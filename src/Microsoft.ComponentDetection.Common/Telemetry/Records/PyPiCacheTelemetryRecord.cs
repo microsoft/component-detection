3f1aa131271d79fe524fcaf04c0625f4f46a1baf
Coby Allred <cobya@github.com>
2022-03-24 15:22:15 -0700

    StyleCop

--- src/Microsoft.ComponentDetection.Common/Telemetry/Records/PyPiCacheTelemetryRecord.cs
+++ src/Microsoft.ComponentDetection.Common/Telemetry/Records/PyPiCacheTelemetryRecord.cs
@@ -7,12 +7,12 @@ namespace Microsoft.ComponentDetection.Common.Telemetry.Records
         public override string RecordName => "PyPiCache";
 
         /// <summary>
-        /// Total number of PyPi requests that hit the cache instead of PyPi APIs
+        /// Gets or sets total number of PyPi requests that hit the cache instead of PyPi APIs.
         /// </summary>
         public int NumCacheHits { get; set; }
 
         /// <summary>
-        /// Size of the PyPi cache at class destruction
+        /// Gets or sets the size of the PyPi cache at class destruction.
         /// </summary>
         public int FinalCacheSize { get; set; }
     }
