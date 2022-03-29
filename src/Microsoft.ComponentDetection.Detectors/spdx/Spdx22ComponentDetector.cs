9c00871de81a503dcd1e046f683f896a8b487633
Anton Kovalyov <thebyagent@gmail.com>
2022-03-29 13:49:24 -0700

    Initial implementation of SPDX22 Detector (#81)
    * Add SPDX detector code and basic tests
    
    * update test.
    
    * Remove version property and add suppressions for intentinal warnings. Add test spdx file for verification tests.
    
    * PR feedback.

---
+++ src/Microsoft.ComponentDetection.Detectors/spdx/Spdx22ComponentDetector.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Microsoft.ComponentDetection.Contracts;
+using Microsoft.ComponentDetection.Contracts.Internal;
+using Microsoft.ComponentDetection.Contracts.TypedComponent;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.ComponentDetection.Detectors.Spdx
+{
+    /// <summary>
+    /// Spdx22ComponentDetector discover SPDX SBOM files in JSON format and create components with the information about
+    /// what SPDX document describes. 
+    /// </summary>
+    [Export(typeof(IComponentDetector))]
+    public class Spdx22ComponentDetector : FileComponentDetector, IDefaultOffComponentDetector 
+    {
+        public override string Id => "SPDX22SBOM";
+        
+        public override IEnumerable<string> Categories =>
+            new[] { Enum.GetName(typeof(DetectorClass), DetectorClass.Spdx) };
+        
+        public override IEnumerable<ComponentType> SupportedComponentTypes { get; } = new[] { ComponentType.Spdx };
+
+        public override int Version => 1;
+        
+        public override IList<string> SearchPatterns { get; } = new List<string> { "*.spdx.json" };
+
+        private readonly IEnumerable<string> supportedSPDXVersions = new List<string> { "SPDX-2.2" };
+
+        protected override Task OnFileFound(ProcessRequest processRequest, IDictionary<string, string> detectorArgs)
+        {
+            Logger.LogVerbose($"Discovered SPDX2.2 manifest file at: {processRequest.ComponentStream.Location}");
+            var file = processRequest.ComponentStream;
+
+            try
+            {
+                var hash = GetSHA1HashFromStream(file.Stream);
+
+                // Reset buffer to starting position after hash generation.
+                file.Stream.Seek(0, SeekOrigin.Begin);
+
+                using var sr = new StreamReader(file.Stream);
+                using var reader = new JsonTextReader(sr);
+                var serializer = new JsonSerializer();
+
+                try
+                {
+                    var document = serializer.Deserialize<JObject>(reader);
+                    if (document != null)
+                    {
+                        if (IsSPDXVersionSupported(document))
+                        {
+                            var sbomComponent = ConvertJObjectToSbomComponent(processRequest, document, hash);
+                            processRequest.SingleFileComponentRecorder.RegisterUsage(new DetectedComponent(sbomComponent));
+                        }
+                        else
+                        {
+                            Logger.LogWarning($"Discovered SPDX at {processRequest.ComponentStream.Location} is not SPDX-2.2 document, skipping");
+                        }
+                    }
+                    else
+                    {
+                        Logger.LogWarning($"Discovered SPDX file at {processRequest.ComponentStream.Location} is not a valid document, skipping");
+                    }
+                }
+                catch (JsonReaderException)
+                {
+                    Logger.LogWarning($"Unable to parse file at {processRequest.ComponentStream.Location}, skipping");
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.LogFailedReadingFile(file.Location, e);
+            }
+
+            return Task.CompletedTask;
+        }
+        
+        private bool IsSPDXVersionSupported(JObject document) => supportedSPDXVersions.Contains(document["spdxVersion"]?.ToString(), StringComparer.OrdinalIgnoreCase);
+
+        private SpdxComponent ConvertJObjectToSbomComponent(ProcessRequest processRequest, JObject document, string fileHash)
+        {
+            var sbomNamespace = document["documentNamespace"]?.ToString();
+            var rootElements = document["documentDescribes"]?.ToObject<string[]>();
+            var name = document["name"]?.ToString();
+            var spdxVersion = document["spdxVersion"]?.ToString();
+
+            if (rootElements != null && rootElements.Count() > 1)
+            {
+                Logger.LogWarning($"SPDX file at {processRequest.ComponentStream.Location} has more than one element in documentDescribes, first will be selected as root element.");
+            }
+            
+            if (rootElements != null && rootElements.Any())
+            {
+                Logger.LogWarning($"SPDX file at {processRequest.ComponentStream.Location} does not have root elements in documentDescribes section, considering SPDXRef-Document as a root element.");
+            }
+
+            var rootElementId = rootElements?.FirstOrDefault() ?? "SPDXRef-Document";
+            var component = new SpdxComponent(spdxVersion, new Uri(sbomNamespace), name, fileHash, rootElementId);
+
+            return component;
+        }
+
+        private string GetSHA1HashFromStream(Stream stream)
+        {
+#pragma warning disable CA5350 // Suppress Do Not Use Weak Cryptographic Algorithms because we use SHA1 intentionally in SPDX format
+            return BitConverter.ToString(SHA1.Create().ComputeHash(stream)).Replace("-", string.Empty).ToLower();
+#pragma warning restore CA5350
+        }
+    }
+}
---
+++ test/Microsoft.ComponentDetection.Detectors.Tests/SPDX22ComponentDetectorTests.cs
@@ -0,0 +1,159 @@
+using Microsoft.ComponentDetection.Common.DependencyGraph;
+using Microsoft.ComponentDetection.Contracts;
+using Microsoft.ComponentDetection.TestsUtilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ComponentDetection.Contracts.TypedComponent;
+using Microsoft.ComponentDetection.Detectors.Spdx;
+
+namespace Microsoft.ComponentDetection.Detectors.Tests
+{
+    [TestClass]
+    [TestCategory("Governance/All")]
+    [TestCategory("Governance/ComponentDetection")]
+    public class Spdx22ComponentDetectorTests
+    {
+        private DetectorTestUtility<Spdx22ComponentDetector> detectorTestUtility;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            var componentRecorder = new ComponentRecorder(enableManualTrackingOfExplicitReferences: false);
+            detectorTestUtility = DetectorTestUtilityCreator.Create<Spdx22ComponentDetector>()
+                                    .WithScanRequest(new ScanRequest(new DirectoryInfo(Path.GetTempPath()), null, null, new Dictionary<string, string>(), null, componentRecorder));
+        }
+
+        [TestMethod]
+        public async Task TestSbomDetector_SimpleSbom()
+        {
+            var spdxFile = @"{
+    ""files"": [{
+        ""fileName"": ""./.eslintrc.js"",
+        ""SPDXID"": ""SPDXRef-File--.eslintrc.js-76586927C59544FB23BE1CF4D269882217EE21AB"",
+        ""checksums"": [
+            {
+                ""algorithm"": ""SHA256"",
+                ""checksumValue"": ""5c9c9d7eb9d31320bd621b3089ec0ae87d97e9ee7ed03cde2d20383928f72958""
+            },
+            {
+                ""algorithm"": ""SHA1"",
+                ""checksumValue"": ""76586927c59544fb23be1cf4d269882217ee21ab""
+            }
+        ],
+        ""licenseConcluded"": ""NOASSERTION"",
+        ""licenseInfoInFile"": [
+            ""NOASSERTION""
+        ],
+        ""copyrightText"": ""NOASSERTION""
+    }],
+    ""packages"": [
+        {
+            ""name"": ""Test"",
+            ""SPDXID"": ""SPDXRef-RootPackage"",
+            ""downloadLocation"": ""NOASSERTION"",
+            ""packageVerificationCode"": {
+                ""packageVerificationCodeValue"": ""12fa1211046c12118936384b6c8683f1ac9b790a""
+            },
+            ""filesAnalyzed"": true,
+            ""licenseConcluded"": ""NOASSERTION"",
+            ""licenseInfoFromFiles"": [
+                ""NOASSERTION""
+            ],
+            ""licenseDeclared"": ""NOASSERTION"",
+            ""copyrightText"": ""NOASSERTION"",
+            ""versionInfo"": ""1.0.0"",
+            ""supplier"": ""Organization: Microsoft"",
+            ""hasFiles"": [""SPDXRef-File--.eslintrc.js-76586927C59544FB23BE1CF4D269882217EE21AB""]
+        }
+    ],
+    ""relationships"": [
+        {
+            ""relationshipType"": ""DESCRIBES"",
+            ""relatedSpdxElement"": ""SPDXRef-RootPackage"",
+            ""spdxElementId"": ""SPDXRef-DOCUMENT""
+        }
+    ],
+    ""spdxVersion"": ""SPDX-2.2"",
+    ""dataLicense"": ""CC0-1.0"",
+    ""SPDXID"": ""SPDXRef-DOCUMENT"",
+    ""name"": ""Test 1.0.0"",
+    ""documentNamespace"": ""https://sbom.microsoft/Test/1.0.0/61de1a5-57cc-4732-9af5-edb321b4a7ee"",
+    ""creationInfo"": {
+        ""created"": ""2022-02-14T20:26:41Z"",
+        ""creators"": [
+            ""Organization: Microsoft"",
+            ""Tool: Microsoft.SBOMTool-1.0.0""
+        ]
+    },
+    ""documentDescribes"": [
+        ""SPDXRef-RootPackage""
+    ]
+}";
+
+            var (scanResult, componentRecorder) = await detectorTestUtility
+                                                    .WithFile("manifest.spdx.json", spdxFile)
+                                                    .ExecuteDetector();
+
+            Assert.AreEqual(ProcessingResultCode.Success, scanResult.ResultCode);
+
+            var detectedComponents = componentRecorder.GetDetectedComponents();
+            var components = detectedComponents.ToList();
+            var sbomComponent = (SpdxComponent)components.FirstOrDefault()?.Component;
+
+            if (sbomComponent is null)
+            {
+                throw new AssertFailedException($"{nameof(sbomComponent)} is null");
+            }
+
+#pragma warning disable CA5350 // Suppress Do Not Use Weak Cryptographic Algorithms because we use SHA1 intentionally in SPDX format
+            var checksum = BitConverter.ToString(SHA1.Create().ComputeHash(
+                Encoding.UTF8.GetBytes(spdxFile))).Replace("-", string.Empty).ToLower();
+#pragma warning restore CA5350
+
+            Assert.AreEqual(1, components.Count());
+            Assert.AreEqual(sbomComponent.Name, "Test 1.0.0");
+            Assert.AreEqual(sbomComponent.RootElementId, "SPDXRef-RootPackage");
+            Assert.AreEqual(sbomComponent.DocumentNamespace,  new Uri("https://sbom.microsoft/Test/1.0.0/61de1a5-57cc-4732-9af5-edb321b4a7ee"));
+            Assert.AreEqual(sbomComponent.SpdxVersion, "SPDX-2.2");
+            Assert.AreEqual(sbomComponent.Checksum, checksum);
+        }
+
+        [TestMethod]
+        public async Task TestSbomDetector_BlankJson()
+        {
+            var spdxFile = "{}";
+            
+            var (scanResult, componentRecorder) = await detectorTestUtility
+                .WithFile("manifest.spdx.json", spdxFile)
+                .ExecuteDetector();
+
+            Assert.AreEqual(ProcessingResultCode.Success, scanResult.ResultCode);
+
+            var detectedComponents = componentRecorder.GetDetectedComponents();
+            var components = detectedComponents.ToList();
+            Assert.IsFalse(components.Any());
+        }
+
+        [TestMethod]
+        public async Task TestSbomDetector_InvalidFile()
+        {
+            var spdxFile = "invalidspdxfile";
+            
+            var (scanResult, componentRecorder) = await detectorTestUtility
+                .WithFile("manifest.spdx.json", spdxFile)
+                .ExecuteDetector();
+
+            Assert.AreEqual(ProcessingResultCode.Success, scanResult.ResultCode);
+
+            var detectedComponents = componentRecorder.GetDetectedComponents();
+            var components = detectedComponents.ToList();
+            Assert.IsFalse(components.Any()); 
+        }
+    }
+}
