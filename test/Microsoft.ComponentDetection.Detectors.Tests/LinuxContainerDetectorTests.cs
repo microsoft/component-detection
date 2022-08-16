37c380aa934159fff349b266be5932e570c0a9b3
Jamie Magee <jamie.magee@gmail.com>
2022-08-16 11:49:41 -0700

    refactor: fix SA1001
    Related to #202
    
    Commas should not be preceded by whitespace
    https://github.com/DotNetAnalyzers/StyleCopAnalyzers/blob/master/documentation/SA1001.md

--- test/Microsoft.ComponentDetection.Detectors.Tests/LinuxContainerDetectorTests.cs
+++ test/Microsoft.ComponentDetection.Detectors.Tests/LinuxContainerDetectorTests.cs
@@ -215,7 +215,7 @@ namespace Microsoft.ComponentDetection.Detectors.Tests
                 .Throws(new IOException());
             this.mockDockerService.Setup(service => service.InspectImageAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()))
                 // Specify BaseImageRef = scratch to verify that cope
-                .ReturnsAsync(new ContainerDetails { Id = 1, ImageId = NodeLatestDigest, Layers = Enumerable.Empty<DockerLayer>() , BaseImageRef = "scratch"});
+                .ReturnsAsync(new ContainerDetails { Id = 1, ImageId = NodeLatestDigest, Layers = Enumerable.Empty<DockerLayer>(), BaseImageRef = "scratch"});
             await this.TestLinuxContainerDetector();
         }
     }
