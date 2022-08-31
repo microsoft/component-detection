053737e38541a54e220431b08448be1dbc9248cd
amitla1 <46578839+amitla1@users.noreply.github.com>
2022-08-31 16:56:22 -0700

    Update YarnParserTests.cs

--- test/Microsoft.ComponentDetection.Detectors.Tests/YarnParserTests.cs
+++ test/Microsoft.ComponentDetection.Detectors.Tests/YarnParserTests.cs
@@ -93,14 +93,6 @@ namespace Microsoft.ComponentDetection.Detectors.Tests
                 {
                     this.CreateDependencyBlock(new Dictionary<string, string> { { "xyz", "2.4" }, { "a", "^1.0.0" } }),
                 }),
-                this.CreateBlock("a@^1.0.0", "1.0.0", "https://a", new List<YarnBlock>
-                {
-                    this.CreateDependencyBlock(new Dictionary<string, string> { { "xyz", "2" } }),
-                }),
-                this.CreateBlock("b@2.4.6", "2.4.6", "https://b", new List<YarnBlock>
-                {
-                    this.CreateDependencyBlock(new Dictionary<string, string> { { "xyz", "2.4" }, { "a", "^1.0.0" } }),
-                }),
                 this.CreateBlock("xyz@2, xyz@2.4", "2.4.3", "https://xyz", Enumerable.Empty<YarnBlock>()),
             };
 
