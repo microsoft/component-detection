5f34f35a3ec09a9f3c89cad384b674be130b186e
Rushabh <rbhansali@microsoft.com>
2022-10-19 15:45:12 -0700

    Update VerificationTest.ps1

--- test/Microsoft.ComponentDetection.VerificationTests/resources/VerificationTest.ps1
+++ test/Microsoft.ComponentDetection.VerificationTests/resources/VerificationTest.ps1
@@ -24,7 +24,7 @@ function main()
     $CDRelease =  $testDataDir + "\component-detection-release"
     $output = $testDataDir + "\output"
     $releaseOutput = $testDataDir + "\release-output"
-    $dockerImagesToScan = "docker.io/library/debian@sha256:9b0e3056b8cd8630271825665a0613cc27829d6a24906dc0122b3b4834312f7d,mcr.microsoft.com/cbl-mariner/base/core:2.0,alpine@sha256:1304f174557314a7ed9eddb4eab12fed12cb0cd9809e4c28f29af86979a3c870"
+    $dockerImagesToScan = "docker.io/library/debian@sha256:9b0e3056b8cd8630271825665a0613cc27829d6a24906dc0122b3b4834312f7d,mcr.microsoft.com/cbl-mariner/base/core:2.0,docker.io/library/alpine@sha256:1304f174557314a7ed9eddb4eab12fed12cb0cd9809e4c28f29af86979a3c870"
 
     Write-Progress "cloning released component-detection at $CDRelease"
     git clone "https://github.com/microsoft/component-detection.git" $CDRelease
