3cf13461f1793dd221c547132d1bdb6785ef5126
Justin Perez <justinperez@microsoft.com>
2023-02-24 10:19:06 -0800

    fix: update documentation

--- test/Microsoft.ComponentDetection.TestsUtilities/ExtensionMethods.cs
+++ test/Microsoft.ComponentDetection.TestsUtilities/ExtensionMethods.cs
@@ -19,10 +19,10 @@ public static class ExtensionMethods
     }
 
     /// <summary>
-    /// Sets up a mock logger to capture the log output to a <see cref="StringBuilder"/>.
+    /// Sets up a mock logger to capture the log output to a <see cref="ICollection{T}"/>.
     /// </summary>
     /// <param name="mockLogger">The mock logger to save the output from.</param>
-    /// <param name="logOutput">A string builder to append the log output to. Each log out it appended as a line.</param>
+    /// <param name="logOutput">A collection to append the log output to. Each log is appended as an element in the collection.</param>
     /// <typeparam name="T">The type of the logger.</typeparam>
     public static void CaptureLogOutput<T>(this Mock<ILogger<T>> mockLogger, ICollection<string> logOutput) =>
         mockLogger.Setup(x => x.Log(
