fb0181fcbdcc25ce690010a5b5d0cab7ca182b07
Justin Perez <justinperez@microsoft.com>
2023-04-11 15:43:50 -0700

    add experiment configs

---
+++ src/Microsoft.ComponentDetection.Orchestrator/Experiments/Configs/NewNugetExperiment.cs
@@ -0,0 +1,22 @@
+﻿namespace Microsoft.ComponentDetection.Orchestrator.Experiments.Configs;
+
+using Microsoft.ComponentDetection.Common.Experiments;
+using Microsoft.ComponentDetection.Contracts;
+using Microsoft.ComponentDetection.Detectors.NuGet;
+
+/// <summary>
+/// Comparing the new NuGet detector approach to the old one.
+/// </summary>
+public class NewNugetExperiment : IExperimentConfiguration
+{
+    /// <inheritdoc />
+    public string Name => "NewNugetDetector";
+
+    /// <inheritdoc />
+    public bool IsInControlGroup(IComponentDetector componentDetector) =>
+        componentDetector is NuGetComponentDetector or NuGetProjectModelProjectCentricComponentDetector;
+
+    /// <inheritdoc />
+    public bool IsInExperimentGroup(IComponentDetector componentDetector) =>
+        componentDetector is NuGetComponentDetector or NuGetPackagesConfigDetector;
+}
