79b755b6e424babde8a394923d6989554acc488f
Justin Perez <justinperez@microsoft.com>
2023-04-13 10:24:23 -0700

    test: add experiment test utils

---
+++ test/Microsoft.ComponentDetection.Orchestrator.Tests/Experiments/ExperimentTestUtils.cs
@@ -0,0 +1,23 @@
+﻿namespace Microsoft.ComponentDetection.Orchestrator.Tests.Experiments;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using Microsoft.ComponentDetection.Contracts;
+using Microsoft.ComponentDetection.Contracts.TypedComponent;
+using Microsoft.ComponentDetection.Orchestrator.Experiments.Models;
+
+public static class ExperimentTestUtils
+{
+    public static DetectedComponent CreateRandomComponent() => new(new NpmComponent(Guid.NewGuid().ToString(), CreateRandomVersion()));
+
+    public static List<DetectedComponent> CreateRandomComponents(int length = 5) =>
+        Enumerable.Range(0, length).Select(_ => CreateRandomComponent()).ToList();
+
+    public static List<ExperimentComponent> CreateRandomExperimentComponents(int length = 5) =>
+        CreateRandomComponents(length).Select(x => new ExperimentComponent(x)).ToList();
+
+    private static string CreateRandomVersion() =>
+        $"{RandomNumberGenerator.GetInt32(0, 100)}.{RandomNumberGenerator.GetInt32(0, 100)}.{RandomNumberGenerator.GetInt32(0, 100)}";
+}
