a1d0e0289087f1429019450e8fc86a306d698b1b
Justin Perez <justinperez@microsoft.com>
2023-04-13 10:25:33 -0700

    test: `Experiment`

---
+++ test/Microsoft.ComponentDetection.Orchestrator.Tests/Experiments/ExperimentTests.cs
@@ -0,0 +1,35 @@
+﻿namespace Microsoft.ComponentDetection.Orchestrator.Tests.Experiments;
+
+using FluentAssertions;
+using Microsoft.ComponentDetection.Orchestrator.Experiments.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+[TestCategory("Governance/All")]
+[TestCategory("Governance/ComponentDetection")]
+public class ExperimentTests
+{
+    [TestMethod]
+    public void Experiment_AddsToOnlyControlGroup()
+    {
+        var experiment = new Experiment();
+        var testComponents = ExperimentTestUtils.CreateRandomComponents();
+
+        experiment.AddComponentsToControlGroup(testComponents);
+
+        experiment.ControlGroupComponents.Should().HaveCount(testComponents.Count);
+        experiment.ExperimentGroupComponents.Should().BeEmpty();
+    }
+
+    [TestMethod]
+    public void Experiment_AddsToOnlyExperimentGroup()
+    {
+        var experiment = new Experiment();
+        var testComponents = ExperimentTestUtils.CreateRandomComponents();
+
+        experiment.AddComponentsToExperimentalGroup(testComponents);
+
+        experiment.ControlGroupComponents.Should().BeEmpty();
+        experiment.ExperimentGroupComponents.Should().HaveCount(testComponents.Count);
+    }
+}
