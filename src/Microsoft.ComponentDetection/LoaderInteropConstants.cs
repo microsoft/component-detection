f59dd48717a5c2958fe499887c039c2713b5eb2f
Jamie Magee <jamagee@microsoft.com>
2023-04-21 08:35:30 -0700

    refactor: delete `LoaderInteropConstants`
    This was required before the migration to dependency injection

--- src/Microsoft.ComponentDetection/LoaderInteropConstants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Microsoft.ComponentDetection.Loader;
-
-public static class LoaderInteropConstants
-{
-    public static readonly string OrchestratorAssemblyModule = typeof(Orchestrator.Orchestrator).Assembly.GetName().Name;
-    public static readonly string OrchestratorTypeName = typeof(Orchestrator.Orchestrator).FullName;
-}
