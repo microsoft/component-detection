02639aa5d685ed9b29915f51641eab01db217eb9
Jamie Magee <jamie.magee@gmail.com>
2023-06-06 12:06:56 -0700

    feat: use `Newtonsoft.Json` streaming methods

--- src/Microsoft.ComponentDetection.Common/IFileWritingService.cs
+++ src/Microsoft.ComponentDetection.Common/IFileWritingService.cs
@@ -4,18 +4,53 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 
-// All file paths are relative and will replace occurrences of {timestamp} with the shared file timestamp.
+/// <summary>
+/// Provides methods for writing files.
+/// </summary>
 public interface IFileWritingService : IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Initializes the file writing service with the given base path.
+    /// </summary>
+    /// <param name="basePath">The base path to use for all file operations.</param>
     void Init(string basePath);
 
-    void AppendToFile(string relativeFilePath, string text);
+    /// <summary>
+    /// Appends the object to the file as JSON.
+    /// </summary>
+    /// <param name="relativeFilePath">The relative path to the file.</param>
+    /// <param name="obj">The object to append.</param>
+    /// <typeparam name="T">The type of the object to append.</typeparam>
+    void AppendToFile<T>(string relativeFilePath, T obj);
 
+    /// <summary>
+    /// Writes the text to the file.
+    /// </summary>
+    /// <param name="relativeFilePath">The relative path to the file.</param>
+    /// <param name="text">The text to write.</param>
     void WriteFile(string relativeFilePath, string text);
 
+    /// <summary>
+    /// Writes the text to the file.
+    /// </summary>
+    /// <param name="relativeFilePath">The relative path to the file.</param>
+    /// <param name="text">The text to write.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     Task WriteFileAsync(string relativeFilePath, string text);
 
-    void WriteFile(FileInfo relativeFilePath, string text);
+    /// <summary>
+    /// Writes the object to the file as JSON.
+    /// </summary>
+    /// <param name="relativeFilePath">The relative path to the file.</param>
+    /// <param name="obj">The object to write.</param>
+    /// <typeparam name="T">The type of the object to write.</typeparam>
+    void WriteFile<T>(FileInfo relativeFilePath, T obj);
 
+    /// <summary>
+    /// Resolves the complete file path from the given relative file path.
+    /// Replaces occurrences of {timestamp} with the shared file timestamp.
+    /// </summary>
+    /// <param name="relativeFilePath">The relative path to the file.</param>
+    /// <returns>The complete file path.</returns>
     string ResolveFilePath(string relativeFilePath);
 }
