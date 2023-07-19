20773e6a33343f62d4e8fa85d9b9023a75739637
Jamie Magee <jamagee@microsoft.com>
2023-07-19 07:56:22 -0700

    feat: add `YarnBerryTypeConverter` a custom YAML converter for `yarn.lock` (#664)

---
+++ src/Microsoft.ComponentDetection.Detectors/yarn/Contracts/YarnBerryLockfile.cs
@@ -0,0 +1,23 @@
+﻿namespace Microsoft.ComponentDetection.Detectors.Yarn.Contracts;
+
+using System.Collections.Generic;
+using YamlDotNet.Serialization;
+
+/// <summary>
+/// Represents the yarn.lock file for yarn v2+ (berry).
+/// There is no official documentation for the format of the yarn.lock file.
+/// This is based on the source code of https://github.com/yarnpkg/berry/blob/master/packages/yarnpkg-core/sources/Project.ts.
+/// </summary>
+public sealed record YarnBerryLockfile
+{
+    /// <summary>
+    /// Gets the metadata of the yarn.lock file.
+    /// </summary>
+    [YamlMember(Alias = "__metadata")]
+    public YarnBerryLockfileMetadata Metadata { get; init; }
+
+    /// <summary>
+    /// Gets the entries of the yarn.lock file.
+    /// </summary>
+    public IDictionary<string, YarnBerryLockfileEntry> Entries { get; init; }
+}
---
+++ src/Microsoft.ComponentDetection.Detectors/yarn/Contracts/YarnBerryLockfileMetadata.cs
@@ -0,0 +1,21 @@
+namespace Microsoft.ComponentDetection.Detectors.Yarn.Contracts;
+
+using YamlDotNet.Serialization;
+
+/// <summary>
+/// Represents the metadata of the yarn.lock file for yarn v2+.
+/// </summary>
+public sealed record YarnBerryLockfileMetadata
+{
+    /// <summary>
+    /// Gets the version of the yarn.lock file.
+    /// </summary>
+    [YamlMember(Alias = "version")]
+    public string Version { get; set; }
+
+    /// <summary>
+    /// Gets the cache key of the yarn.lock file.
+    /// </summary>
+    [YamlMember(Alias = "cacheKey")]
+    public string CacheKey { get; set; }
+}
