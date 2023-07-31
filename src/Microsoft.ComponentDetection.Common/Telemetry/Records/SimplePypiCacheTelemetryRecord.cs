737c33f762571519f6c1be6e607f2889eae5a728
Omotola <oakeredolu@microsoft.com>
2023-07-31 09:12:52 -0700

    New PypiClient using the new Simple Api (#672)
    * Added a new client for SimplePypi and fixed disposal in the original pypi client

---
+++ src/Microsoft.ComponentDetection.Common/Telemetry/Records/SimplePypiCacheTelemetryRecord.cs
@@ -0,0 +1,26 @@
+namespace Microsoft.ComponentDetection.Common.Telemetry.Records;
+
+public class SimplePypiCacheTelemetryRecord : BaseDetectionTelemetryRecord
+{
+    public override string RecordName => "SimplePyPiCache";
+
+    /// <summary>
+    /// Gets or sets total number of PyPi project requests that hit the cache instead of SimplePyPi API.
+    /// </summary>
+    public int NumSimpleProjectCacheHits { get; set; }
+
+    /// <summary>
+    /// Gets or sets total number of project wheel file requests that hit the cache instead of API.
+    /// </summary>
+    public int NumProjectFileCacheHits { get; set; }
+
+    /// <summary>
+    /// Gets or sets the size of the Simple Project cache at class destruction.
+    /// </summary>
+    public int FinalSimpleProjectCacheSize { get; set; }
+
+    /// <summary>
+    /// Gets or sets the size of the Project Wheel File cache at class destruction.
+    /// </summary>
+    public int FinalProjectFileCacheSize { get; set; }
+}
---
+++ src/Microsoft.ComponentDetection.Detectors/pip/ISimplePyPiClient.cs
@@ -0,0 +1,22 @@
+namespace Microsoft.ComponentDetection.Detectors.Pip;
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+public interface ISimplePyPiClient
+{
+    /// <summary>
+    /// Uses the release url to retrieve the project file.
+    /// </summary>
+    /// <param name="releaseUrl">The url to fetch dependencies from. </param>
+    /// <returns>Returns a project from the simplepypi api. </returns>
+    Task<Stream> FetchPackageFileStreamAsync(Uri releaseUrl);
+
+    /// <summary>
+    /// Calls simplepypi and retrieves the project specified with the spec name.
+    /// </summary>
+    /// <param name="spec">The PipDependencySpecification for the project. </param>
+    /// <returns>Returns a project from the simplepypi api. </returns>
+    Task<SimplePypiProject> GetSimplePypiProjectAsync(PipDependencySpecification spec);
+}
