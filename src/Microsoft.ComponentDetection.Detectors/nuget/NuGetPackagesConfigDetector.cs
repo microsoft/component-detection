2192418ed226f93a0740ebe316c01ea46803fe0d
Jamie Magee <jamagee@microsoft.com>
2023-08-18 11:03:36 -0700

    Downgrade invalid `packages.config` files from an error to warning (#730)

--- src/Microsoft.ComponentDetection.Detectors/nuget/NuGetPackagesConfigDetector.cs
+++ src/Microsoft.ComponentDetection.Detectors/nuget/NuGetPackagesConfigDetector.cs
@@ -10,8 +10,17 @@ using Microsoft.ComponentDetection.Contracts.Internal;
 using Microsoft.ComponentDetection.Contracts.TypedComponent;
 using Microsoft.Extensions.Logging;
 
-public class NuGetPackagesConfigDetector : FileComponentDetector
+/// <summary>
+/// Detects NuGet packages in packages.config files.
+/// </summary>
+public sealed class NuGetPackagesConfigDetector : FileComponentDetector
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NuGetPackagesConfigDetector"/> class.
+    /// </summary>
+    /// <param name="componentStreamEnumerableFactory">The factory for handing back component streams to File detectors.</param>
+    /// <param name="walkerFactory">The factory for creating directory walkers.</param>
+    /// <param name="logger">The logger to use.</param>
     public NuGetPackagesConfigDetector(
         IComponentStreamEnumerableFactory componentStreamEnumerableFactory,
         IObservableDirectoryWalkerFactory walkerFactory,
@@ -22,17 +31,23 @@ public class NuGetPackagesConfigDetector : FileComponentDetector
         this.Logger = logger;
     }
 
+    /// <inheritdoc />
     public override IList<string> SearchPatterns => new[] { "packages.config" };
 
+    /// <inheritdoc />
     public override string Id => "NuGetPackagesConfig";
 
+    /// <inheritdoc />
     public override IEnumerable<string> Categories =>
         new[] { Enum.GetName(typeof(DetectorClass), DetectorClass.NuGet) };
 
+    /// <inheritdoc />
     public override IEnumerable<ComponentType> SupportedComponentTypes => new[] { ComponentType.NuGet };
 
+    /// <inheritdoc />
     public override int Version => 1;
 
+    /// <inheritdoc />
     protected override Task OnFileFoundAsync(ProcessRequest processRequest, IDictionary<string, string> detectorArgs)
     {
         try
@@ -52,7 +67,7 @@ public class NuGetPackagesConfigDetector : FileComponentDetector
         }
         catch (Exception e) when (e is PackagesConfigReaderException or XmlException)
         {
-            this.Logger.LogError(e, "Failed to read packages.config file {File}", processRequest.ComponentStream.Location);
+            this.Logger.LogWarning(e, "Failed to read packages.config file {File}", processRequest.ComponentStream.Location);
         }
 
         return Task.CompletedTask;
