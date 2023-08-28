168315b7a1e1d0414832501c5e80c161652a06e1
Tarun Kumar <tarun.mewara06@gmail.com>
2023-08-28 21:38:22 +0530

    Add MD5, SHA1 information to ConanComponent (#733)

--- src/Microsoft.ComponentDetection.Contracts/TypedComponent/ConanComponent.cs
+++ src/Microsoft.ComponentDetection.Contracts/TypedComponent/ConanComponent.cs
@@ -9,15 +9,23 @@ public class ConanComponent : TypedComponent
         // reserved for deserialization
     }
 
-    public ConanComponent(string name, string version)
+    public ConanComponent(string name, string version, string previous, string packageId)
     {
         this.Name = this.ValidateRequiredInput(name, nameof(this.Name), nameof(ComponentType.Conan));
         this.Version = this.ValidateRequiredInput(version, nameof(this.Version), nameof(ComponentType.Conan));
+        this.Md5Hash = this.ValidateRequiredInput(previous, nameof(this.Md5Hash), nameof(ComponentType.Conan));
+        this.Sha1Hash = this.ValidateRequiredInput(packageId, nameof(this.Sha1Hash), nameof(ComponentType.Conan));
     }
 
     public string Name { get; set; }
 
     public string Version { get; set; }
+
+    public string Md5Hash { get; set; }
+
+    public string Sha1Hash { get; set; }
+
+    public string PackageSourceURL => $"https://conan.io/center/recipes/{this.Name}?version={this.Version}";
 
     public override ComponentType Type => ComponentType.Conan;
 
