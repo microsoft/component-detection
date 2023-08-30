16ffad5b092d7d61f718fb7a7264d2e7f1715ee1
Justin Perez <justinperez@microsoft.com>
2023-08-30 12:44:26 -0700

    tests(common): test `PatternMatchingUtility`

---
+++ test/Microsoft.ComponentDetection.Common.Tests/PatternMatchingUtilityTests.cs
@@ -0,0 +1,46 @@
+namespace Microsoft.ComponentDetection.Common.Tests;
+
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+[TestCategory("Governance/All")]
+[TestCategory("Governance/ComponentDetection")]
+public class PatternMatchingUtilityTests
+{
+    [TestMethod]
+    public void PatternMatcher_Matches_StartsWith()
+    {
+        var pattern = "test*";
+        var input = "test123";
+
+        var matcher = PatternMatchingUtility.GetFilePatternMatcher(new[] { pattern });
+
+        matcher(input).Should().BeTrue();
+        matcher("123test").Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void PatternMatcher_Matches_EndsWith()
+    {
+        var pattern = "*test";
+        var input = "123test";
+
+        var matcher = PatternMatchingUtility.GetFilePatternMatcher(new[] { pattern });
+
+        matcher(input).Should().BeTrue();
+        matcher("test123").Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void PatternMatcher_Matches_Exact()
+    {
+        var pattern = "test";
+        var input = "test";
+
+        var matcher = PatternMatchingUtility.GetFilePatternMatcher(new[] { pattern });
+
+        matcher(input).Should().BeTrue();
+        matcher("123test").Should().BeFalse();
+    }
+}
