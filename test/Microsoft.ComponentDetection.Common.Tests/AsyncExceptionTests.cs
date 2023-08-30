dbc661ad074b4422aa2bb16437c24296963c4850
Justin Perez <justinperez@microsoft.com>
2023-08-30 12:17:55 -0700

    tests(common): add `AsyncExecution`

---
+++ test/Microsoft.ComponentDetection.Common.Tests/AsyncExceptionTests.cs
@@ -0,0 +1,57 @@
+namespace Microsoft.ComponentDetection.Common.Tests;
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+[TestCategory("Governance/All")]
+[TestCategory("Governance/ComponentDetection")]
+public class AsyncExceptionTests
+{
+    [TestMethod]
+    public async Task ExecuteWithTimeoutAsync_ThrowsNullExceptionAsync()
+    {
+        Func<Task<int>> toExecute = null;
+
+        var func = async () => await AsyncExecution.ExecuteWithTimeoutAsync(toExecute, TimeSpan.FromSeconds(1), CancellationToken.None);
+
+        await func.Should().ThrowAsync<ArgumentNullException>();
+    }
+
+    [TestMethod]
+    public async Task ExecuteWithoutTimeAsync_ThrowsTimeoutExceptionAsync()
+    {
+        var toExecute = async () =>
+        {
+            await Task.Delay(5000);
+            return 0;
+        };
+
+        var func = async () => await AsyncExecution.ExecuteWithTimeoutAsync(toExecute, TimeSpan.FromSeconds(1), CancellationToken.None);
+
+        await func.Should().ThrowAsync<TimeoutException>();
+    }
+
+    [TestMethod]
+    public async Task ExecuteVoidWithTimeoutAsync_ThrowsNullExceptionAsync()
+    {
+        Action toExecute = null;
+
+        var func = async () => await AsyncExecution.ExecuteVoidWithTimeoutAsync(toExecute, TimeSpan.FromSeconds(1), CancellationToken.None);
+
+        await func.Should().ThrowAsync<ArgumentNullException>();
+    }
+
+    [TestMethod]
+    public async Task ExecuteVoidWithTimeoutAsync_ThrowsTimeoutExceptionAsync()
+    {
+        var toExecute = () => Task.Delay(5000).Wait();
+
+        var func = async () => await AsyncExecution.ExecuteVoidWithTimeoutAsync(toExecute, TimeSpan.FromSeconds(1), CancellationToken.None);
+
+        await func.Should().ThrowAsync<TimeoutException>();
+    }
+}
