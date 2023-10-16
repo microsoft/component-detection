a54cb7cd51d8ecb41d84e37444f9a5ab8321ea75
Amitla Vannikumar <46578839+amitla1@users.noreply.github.com>
2023-10-15 23:56:16 -0700

    Add Properties VCPKG (#855)
    * added properties
    
    * changed download location parsing to sanitize better
    
    * added more checks
    
    ---------
    
    Co-authored-by: Amitla Vannikumar <avannikumar@microsoft.com>

--- src/Microsoft.ComponentDetection.Contracts/TypedComponent/VcpkgComponent.cs
+++ src/Microsoft.ComponentDetection.Contracts/TypedComponent/VcpkgComponent.cs
@@ -1,5 +1,6 @@
-﻿namespace Microsoft.ComponentDetection.Contracts.TypedComponent;
+namespace Microsoft.ComponentDetection.Contracts.TypedComponent;
 
+using System.Linq;
 using PackageUrl;
 
 public class VcpkgComponent : TypedComponent
@@ -20,6 +21,11 @@ public class VcpkgComponent : TypedComponent
         this.Triplet = triplet;
         this.Description = description;
         this.DownloadLocation = downloadLocation;
+
+        if (!string.IsNullOrEmpty(downloadLocation) && downloadLocation.ToLower().Contains("https://github.com/"))
+        {
+            this.SetGitRepoProperties();
+        }
     }
 
     public string SPDXID { get; set; }
@@ -35,6 +41,10 @@ public class VcpkgComponent : TypedComponent
     public string Description { get; set; }
 
     public int PortVersion { get; set; }
+
+    public string GitRepositoryOwner { get; set; }
+
+    public string GitRepositoryName { get; set; }
 
     public override ComponentType Type => ComponentType.Vcpkg;
 
@@ -69,6 +79,24 @@ public class VcpkgComponent : TypedComponent
             {
                 return new PackageURL($"pkg:vcpkg/{this.Name}");
             }
+        }
+    }
+
+    private void SetGitRepoProperties()
+    {
+        /* example download locations
+         * "git+https://github.com/leethomason/tinyxml2@9.0.0"
+         * "git+https://github.com/Microsoft/vcpkg#ports/nlohmann-json"
+        */
+        var locationArr = this.DownloadLocation.Split('/');
+        if (!string.IsNullOrEmpty(locationArr[2]))
+        {
+            this.GitRepositoryOwner = locationArr[2];
+        }
+
+        if (!string.IsNullOrEmpty(locationArr[3]))
+        {
+            this.GitRepositoryName = locationArr[3].TakeWhile(ch => char.IsLetterOrDigit(ch)).ToString();
         }
     }
 }
