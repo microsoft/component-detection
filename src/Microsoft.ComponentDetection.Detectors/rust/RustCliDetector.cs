97c26f6b1ecf88e7eb94f9ee4133e6f01b86871a
Rob Jellinghaus <rjelling@microsoft.com>
2023-11-09 14:41:26 -0800

    Fix formatting.

--- src/Microsoft.ComponentDetection.Detectors/rust/RustCliDetector.cs
+++ src/Microsoft.ComponentDetection.Detectors/rust/RustCliDetector.cs
@@ -64,11 +64,11 @@ public class RustCliDetector : FileComponentDetector, IDefaultOffComponentDetect
                 return;
             }
 
+            // Use --all-features to ensure that even optional feature dependencies are detected.
             var cliResult = await this.cliService.ExecuteCommandAsync(
                 "cargo",
                 null,
                 "metadata",
-                // Use --all-features to ensure that even optional feature dependencies are detected.
                 "--all-features",
                 "--manifest-path",
                 componentStream.Location,
