d3b0d02abfe54bf88006b29e4fa166bd47e8580f
Coby Allred <coallred@microsoft.com>
2024-05-22 14:29:49 -0700

    Don't use primary constructor

--- src/Microsoft.ComponentDetection.Detectors/pip/Contracts/PipReportGraphNode.cs
+++ src/Microsoft.ComponentDetection.Detectors/pip/Contracts/PipReportGraphNode.cs
@@ -7,13 +7,19 @@ using Microsoft.ComponentDetection.Contracts.TypedComponent;
 /// Internal state used by PipReportDetector to hold intermediate structure info until the final
 /// combination of dependencies and relationships is determined and can be returned.
 /// </summary>
-public class PipReportGraphNode(PipComponent component, bool requested)
+public class PipReportGraphNode
 {
-    public PipComponent Value { get; set; } = component;
+    public PipReportGraphNode(PipComponent value, bool requested)
+    {
+        this.Value = value;
+        this.Requested = requested;
+    }
+
+    public PipComponent Value { get; set; }
 
     public List<PipReportGraphNode> Children { get; } = new List<PipReportGraphNode>();
 
     public List<PipReportGraphNode> Parents { get; } = new List<PipReportGraphNode>();
 
-    public bool Requested { get; set; } = requested;
+    public bool Requested { get; set; }
 }
