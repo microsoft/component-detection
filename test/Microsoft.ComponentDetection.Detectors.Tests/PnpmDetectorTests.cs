393db4724c233955ac478279adea5dbd304a1ad8
Paul Dorsch <107068277+pauld-msft@users.noreply.github.com>
2024-06-03 13:02:49 -0400

    Merged pnpm6 experiment into pnpm detector (#1145)
    * merged pnpm6 into pnpm detector
    
    * bump version and make the factory method private
    
    * name of type
    
    * improved logging
    
    * added telemetry record for pnpm, and other minor updates to methods for conciseness
    
    * standardize the invalid version telemetry object
    
    * removed invalid version file

--- test/Microsoft.ComponentDetection.Detectors.Tests/PnpmDetectorTests.cs
+++ test/Microsoft.ComponentDetection.Detectors.Tests/PnpmDetectorTests.cs
@@ -18,7 +18,7 @@ using Moq;
 [TestClass]
 [TestCategory("Governance/All")]
 [TestCategory("Governance/ComponentDetection")]
-public class PnpmDetectorTests : BaseDetectorTest<PnpmComponentDetector>
+public class PnpmDetectorTests : BaseDetectorTest<PnpmComponentDetectorFactory>
 {
     public PnpmDetectorTests()
     {
@@ -394,10 +394,190 @@ packages:
     }
 
     [TestMethod]
-    public async Task TestPnpmDetector_V5_BadLockVersion_EmptyAsync()
+    public async Task TestPnpmDetector_BadLockVersion_EmptyAsync()
+    {
+        var yamlFile = @"
+lockfileVersion: '4.0'
+settings:
+  autoInstallPeers: true
+  excludeLinksFromLockfile: false
+dependencies:
+  renamed:
+    specifier: npm:minimist@*
+    version: /minimist@1.2.8
+packages:
+  /minimist@1.2.8:
+    resolution: {integrity: sha512-2yyAR8qBkN3YuheJanUpWC5U3bb5osDywNB8RzDVlDwDHbocAJveqqj1u8+SVD7jkWT4yvsHCpWqqWqAxb0zCA==}
+    dev: false
+";
+
+        var (scanResult, componentRecorder) = await this.DetectorTestUtility
+            .WithFile("pnpm-lock.yaml", yamlFile)
+            .ExecuteDetectorAsync();
+
+        scanResult.ResultCode.Should().Be(ProcessingResultCode.Success);
+
+        var detectedComponents = componentRecorder.GetDetectedComponents();
+        detectedComponents.Should().BeEmpty();
+    }
+
+    [TestMethod]
+    public async Task TestPnpmDetector_V5_GoodLockVersion_ParsedDependenciesAsync()
+    {
+        var yamlFile = @"
+lockfileVersion: '5.0'
+dependencies:
+  'query-string': 4.3.4,
+  'strict-uri-encode': 1.1.0
+packages:
+  /query-string/4.3.4:
+    dev: false
+  /strict-uri-encode/1.1.0:
+    dev: true";
+
+        var (scanResult, componentRecorder) = await this.DetectorTestUtility
+            .WithFile("pnpm-lock.yaml", yamlFile)
+            .ExecuteDetectorAsync();
+
+        scanResult.ResultCode.Should().Be(ProcessingResultCode.Success);
+
+        var detectedComponents = componentRecorder.GetDetectedComponents();
+        var noDevDependencyComponent = detectedComponents.Select(x => new { Component = x.Component as NpmComponent, DetectedComponent = x }).FirstOrDefault(x => x.Component.Name.Contains("query-string"));
+        var devDependencyComponent = detectedComponents.Select(x => new { Component = x.Component as NpmComponent, DetectedComponent = x }).FirstOrDefault(x => x.Component.Name.Contains("strict-uri-encode"));
+
+        componentRecorder.GetEffectiveDevDependencyValue(noDevDependencyComponent.Component.Id).Should().BeFalse();
+        componentRecorder.GetEffectiveDevDependencyValue(devDependencyComponent.Component.Id).Should().BeTrue();
+    }
+
+    [TestMethod]
+    public async Task TestPnpmDetector_V6_SuccessAsync()
     {
         var yamlFile = @"
 lockfileVersion: '6.0'
+settings:
+  autoInstallPeers: true
+  excludeLinksFromLockfile: false
+dependencies:
+  minimist:
+    specifier: 1.2.8
+    version: 1.2.8
+packages:
+  /minimist@1.2.8:
+    resolution: {integrity: sha512-2yyAR8qBkN3YuheJanUpWC5U3bb5osDywNB8RzDVlDwDHbocAJveqqj1u8+SVD7jkWT4yvsHCpWqqWqAxb0zCA==}
+    dev: false
+";
+
+        var (scanResult, componentRecorder) = await this.DetectorTestUtility
+            .WithFile("pnpm-lock.yaml", yamlFile)
+            .ExecuteDetectorAsync();
+
+        scanResult.ResultCode.Should().Be(ProcessingResultCode.Success);
+
+        var detectedComponents = componentRecorder.GetDetectedComponents();
+        detectedComponents.Should().ContainSingle();
+
+        var minimist = detectedComponents.Single(component => ((NpmComponent)component.Component).Name.Contains("minimist"));
+        componentRecorder.AssertAllExplicitlyReferencedComponents<NpmComponent>(
+            minimist.Component.Id,
+            parentComponent => parentComponent.Name == "minimist");
+
+        componentRecorder.ForAllComponents(grouping => grouping.AllFileLocations.First().Should().Contain("pnpm-lock.yaml"));
+
+        foreach (var component in detectedComponents)
+        {
+            component.Component.Type.Should().Be(ComponentType.Npm);
+        }
+    }
+
+    [TestMethod]
+    public async Task TestPnpmDetector_V6_WorkspaceAsync()
+    {
+        var yamlFile = @"
+lockfileVersion: '6.0'
+settings:
+  autoInstallPeers: true
+  excludeLinksFromLockfile: false
+importers:
+  .:
+    dependencies:
+      minimist:
+        specifier: 1.2.8
+        version: 1.2.8
+packages:
+  /minimist@1.2.8:
+    resolution: {integrity: sha512-2yyAR8qBkN3YuheJanUpWC5U3bb5osDywNB8RzDVlDwDHbocAJveqqj1u8+SVD7jkWT4yvsHCpWqqWqAxb0zCA==}
+    dev: false
+";
+
+        var (scanResult, componentRecorder) = await this.DetectorTestUtility
+            .WithFile("pnpm-lock.yaml", yamlFile)
+            .ExecuteDetectorAsync();
+
+        scanResult.ResultCode.Should().Be(ProcessingResultCode.Success);
+
+        var detectedComponents = componentRecorder.GetDetectedComponents();
+        detectedComponents.Should().ContainSingle();
+
+        var minimist = detectedComponents.Single(component => ((NpmComponent)component.Component).Name.Contains("minimist"));
+        componentRecorder.AssertAllExplicitlyReferencedComponents<NpmComponent>(
+            minimist.Component.Id,
+            parentComponent => parentComponent.Name == "minimist");
+
+        componentRecorder.ForAllComponents(grouping => grouping.AllFileLocations.First().Should().Contain("pnpm-lock.yaml"));
+
+        foreach (var component in detectedComponents)
+        {
+            component.Component.Type.Should().Be(ComponentType.Npm);
+        }
+    }
+
+    // Test that renamed package is handled correctly, and that resolved version gets used (not specifier)
+    [TestMethod]
+    public async Task TestPnpmDetector_V6_RenamedAsync()
+    {
+        var yamlFile = @"
+lockfileVersion: '6.0'
+settings:
+  autoInstallPeers: true
+  excludeLinksFromLockfile: false
+dependencies:
+  renamed:
+    specifier: npm:minimist@*
+    version: /minimist@1.2.8
+packages:
+  /minimist@1.2.8:
+    resolution: {integrity: sha512-2yyAR8qBkN3YuheJanUpWC5U3bb5osDywNB8RzDVlDwDHbocAJveqqj1u8+SVD7jkWT4yvsHCpWqqWqAxb0zCA==}
+    dev: false
+";
+
+        var (scanResult, componentRecorder) = await this.DetectorTestUtility
+            .WithFile("pnpm-lock.yaml", yamlFile)
+            .ExecuteDetectorAsync();
+
+        scanResult.ResultCode.Should().Be(ProcessingResultCode.Success);
+
+        var detectedComponents = componentRecorder.GetDetectedComponents();
+        detectedComponents.Should().ContainSingle();
+
+        var minimist = detectedComponents.Single(component => ((NpmComponent)component.Component).Name.Equals("minimist"));
+        componentRecorder.AssertAllExplicitlyReferencedComponents<NpmComponent>(
+            minimist.Component.Id,
+            parentComponent => parentComponent.Name == "minimist");
+        ((NpmComponent)minimist.Component).Version.Should().BeEquivalentTo("1.2.8");
+
+        componentRecorder.ForAllComponents(grouping => grouping.AllFileLocations.First().Should().Contain("pnpm-lock.yaml"));
+
+        foreach (var component in detectedComponents)
+        {
+            component.Component.Type.Should().Be(ComponentType.Npm);
+        }
+    }
+
+    [TestMethod]
+    public async Task TestPnpmDetector_V6_BadLockVersion_EmptyAsync()
+    {
+        var yamlFile = @"
+lockfileVersion: '5.0'
 settings:
   autoInstallPeers: true
   excludeLinksFromLockfile: false
