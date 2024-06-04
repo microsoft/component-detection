9f1e5a9083161fb47a44c13873eab9d8ac72e7d9
Coby Allred <cobyallred@gmail.com>
2024-06-04 15:10:30 -0700

    Update NormalizePath to be more robust (#1152)

--- test/Microsoft.ComponentDetection.Common.Tests/PathUtilityServiceTests.cs
+++ test/Microsoft.ComponentDetection.Common.Tests/PathUtilityServiceTests.cs
@@ -1,6 +1,5 @@
 namespace Microsoft.ComponentDetection.Common.Tests;
 
-using System.IO;
 using FluentAssertions;
 using Microsoft.ComponentDetection.Common;
 using Microsoft.Extensions.Logging.Abstractions;
@@ -16,14 +15,20 @@ public class PathUtilityServiceTests
     {
         var service = new PathUtilityService(new NullLogger<PathUtilityService>());
         var path = "Users\\SomeUser\\someDir\\someFile";
+        var expectedPath = "Users/SomeUser/someDir/someFile";
         var normalizedPath = service.NormalizePath(path);
-        if (Path.DirectorySeparatorChar == '\\')
-        {
-            normalizedPath.Should().Be(path);
-        }
-        else
-        {
-            normalizedPath.Should().Be(string.Join(Path.DirectorySeparatorChar, path.Split('\\')));
-        }
+
+        normalizedPath.Should().Be(expectedPath);
+    }
+
+    [TestMethod]
+    public void AbsolutePathShouldBeNormalized()
+    {
+        var service = new PathUtilityService(new NullLogger<PathUtilityService>());
+        var path = "C:\\Users\\SomeUser\\someDir\\someFile";
+        var expectedPath = "C:/Users/SomeUser/someDir/someFile";
+        var normalizedPath = service.NormalizePath(path);
+
+        normalizedPath.Should().Be(expectedPath);
     }
 }
