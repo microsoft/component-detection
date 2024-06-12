22ca097249df3dae0a46d01386859f123a4a0cb9
Paul Dorsch <pauldorsch@microsoft.com>
2024-06-12 16:43:04 -0700

    reduce default parallelism

--- src/Microsoft.ComponentDetection.Orchestrator/Services/DetectorProcessingService.cs
+++ src/Microsoft.ComponentDetection.Orchestrator/Services/DetectorProcessingService.cs
@@ -23,7 +23,7 @@ using static System.Environment;
 
 public class DetectorProcessingService : IDetectorProcessingService
 {
-    private const int DefaultMaxDetectionThreads = 10;
+    private const int DefaultMaxDetectionThreads = 3;
 
     private readonly IObservableDirectoryWalkerFactory scanner;
     private readonly ILogger<DetectorProcessingService> logger;
