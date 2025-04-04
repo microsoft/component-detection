0f198be2e172794e518d70f996f7eca538d5903c
Paul Dorsch <pauldorsch@microsoft.com>
2025-04-04 12:00:14 -0400

    remove the large list of everchanging deps from the python scans

--- test/Microsoft.ComponentDetection.VerificationTests/resources/VerificationTest.ps1
+++ test/Microsoft.ComponentDetection.VerificationTests/resources/VerificationTest.ps1
@@ -42,7 +42,8 @@ function main()
     dotnet run scan --SourceDirectory $verificationTestRepo --Output $output `
                     --DockerImagesToScan $dockerImagesToScan `
                     --DetectorArgs DockerReference=EnableIfDefaultOff,SPDX22SBOM=EnableIfDefaultOff,CondaLock=EnableIfDefaultOff,ConanLock=EnableIfDefaultOff `
-                    --MaxDetectionThreads 5 --DebugTelemetry
+                    --MaxDetectionThreads 5 --DebugTelemetry `
+                    --DirectoryExclusionList "**/pip/parallel/**;**/pip/roots/**;**/pip/pre-generated/**"
 
     Set-Location $CDRelease
     dotnet restore
@@ -50,7 +51,8 @@ function main()
     dotnet run scan --SourceDirectory $verificationTestRepo --Output $releaseOutput `
                     --DockerImagesToScan $dockerImagesToScan `
                     --DetectorArgs DockerReference=EnableIfDefaultOff,SPDX22SBOM=EnableIfDefaultOff,CondaLock=EnableIfDefaultOff,ConanLock=EnableIfDefaultOff `
-                    --MaxDetectionThreads 5 --DebugTelemetry
+                    --MaxDetectionThreads 5 --DebugTelemetry `
+                    --DirectoryExclusionList "**/pip/parallel/**;**/pip/roots/**;**/pip/pre-generated/**"
 
     $env:GITHUB_OLD_ARTIFACTS_DIR = $releaseOutput
     $env:GITHUB_NEW_ARTIFACTS_DIR = $output
