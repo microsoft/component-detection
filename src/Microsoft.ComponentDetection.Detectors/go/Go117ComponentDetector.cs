e747d58008359009491846899a0fd29d1a94877e
Aayush Maini <aamaini@microsoft.com>
2025-05-21 09:49:05 -0700

    Use Go CLI scan instead of go.sum if available

--- src/Microsoft.ComponentDetection.Detectors/go/Go117ComponentDetector.cs
+++ src/Microsoft.ComponentDetection.Detectors/go/Go117ComponentDetector.cs
@@ -97,7 +97,10 @@ public class Go117ComponentDetector : FileComponentDetector, IExperimentalDetect
             return;
         }
 
-        var record = new GoGraphTelemetryRecord();
+        using var record = new GoGraphTelemetryRecord();
+        record.WasGoCliDisabled = false;
+        record.WasGoFallbackStrategyUsed = false;
+
         var fileExtension = Path.GetExtension(file.Location).ToUpperInvariant();
         switch (fileExtension)
         {
@@ -123,7 +126,24 @@ public class Go117ComponentDetector : FileComponentDetector, IExperimentalDetect
             case ".SUM":
             {
                 this.Logger.LogDebug("Found Go.sum: {Location}", file.Location);
-                await this.goParserFactory.CreateParser(GoParserType.GoSum, this.Logger).ParseAsync(singleFileComponentRecorder, file, record);
+
+                // check if we can use Go CLI instead
+                var wasGoCliScanSuccessful = false;
+                if (!this.IsGoCliManuallyDisabled())
+                {
+                    wasGoCliScanSuccessful = await this.goParserFactory.CreateParser(GoParserType.GoCLI, this.Logger).ParseAsync(singleFileComponentRecorder, file, record);
+                }
+
+                this.Logger.LogDebug("Status of Go CLI scan when considering {GoSumLocation}: {Status}", file.Location, wasGoCliScanSuccessful);
+
+                // If Go CLI scan was not successful/disabled, scan go.sum because this go.sum was recorded due to go.mod
+                // containing go < 1.17. So go.mod is incomplete. We need to parse go.sum to make list of dependencies complete
+                if (!wasGoCliScanSuccessful)
+                {
+                    this.Logger.LogDebug("Go CLI scan when considering {GoSumLocation} was not successful. Falling back to scanning go.sum", file.Location);
+                    await this.goParserFactory.CreateParser(GoParserType.GoSum, this.Logger).ParseAsync(singleFileComponentRecorder, file, record);
+                }
+
                 break;
             }
 
