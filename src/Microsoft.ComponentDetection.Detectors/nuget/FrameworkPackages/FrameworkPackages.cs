5ef3748aabb754144f6e6dd466f1f2574ab86cca
Eric StJohn <ericstj@microsoft.com>
2025-07-22 19:02:00 -0700

    Fix typo

--- src/Microsoft.ComponentDetection.Detectors/nuget/FrameworkPackages/FrameworkPackages.cs
+++ src/Microsoft.ComponentDetection.Detectors/nuget/FrameworkPackages/FrameworkPackages.cs
@@ -217,7 +217,7 @@ internal sealed partial class FrameworkPackages : IEnumerable<KeyValuePair<strin
 
     private void Add(string id, string version)
     {
-        if (string.IsNullOrWhitespace(version))
+        if (string.IsNullOrWhiteSpace(version))
         {
             this.Packages.Remove(id);
         }
