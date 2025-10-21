777711febdd4ba6f6798d1e1567682d33d410b03
Eric StJohn <ericstj@microsoft.com>
2025-10-21 15:39:47 -0700

    Don't trim the root separator

--- src/Microsoft.ComponentDetection.Detectors/dotnet/DotNetComponentDetector.cs
+++ src/Microsoft.ComponentDetection.Detectors/dotnet/DotNetComponentDetector.cs
@@ -58,9 +58,22 @@ public class DotNetComponentDetector : FileComponentDetector
 
     public override IEnumerable<string> Categories => ["DotNet"];
 
+    private static string TrimAllEndingDirectorySeparators(string path)
+    {
+        string last;
+
+        do
+        {
+            last = path;
+            path = Path.TrimEndingDirectorySeparator(last);
+        }
+        while (!ReferenceEquals(last, path));
+
+        return path;
+    }
+
     [return: NotNullIfNotNull(nameof(path))]
-    private string? NormalizeDirectory(string? path) => string.IsNullOrEmpty(path) ? path :
-        this.pathUtilityService.NormalizePath(path).TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
+    private string? NormalizeDirectory(string? path) => string.IsNullOrEmpty(path) ? path : TrimAllEndingDirectorySeparators(this.pathUtilityService.NormalizePath(path));
 
     /// <summary>
     /// Given a path under sourceDirectory, and the same path in another filesystem,
