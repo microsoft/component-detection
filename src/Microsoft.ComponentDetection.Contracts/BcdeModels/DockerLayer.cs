a009fc83dfc83b8bb04dd918b6ec666beb76ef65
Jamie Magee <jamie.magee@gmail.com>
2025-11-21 13:43:11 -0800

    Add explicit `JsonPropertyNameAttribute` to models

--- src/Microsoft.ComponentDetection.Contracts/BcdeModels/DockerLayer.cs
+++ src/Microsoft.ComponentDetection.Contracts/BcdeModels/DockerLayer.cs
@@ -1,22 +1,28 @@
 #nullable disable
 namespace Microsoft.ComponentDetection.Contracts.BcdeModels;
 
+using System.Text.Json.Serialization;
+
 public class DockerLayer
 {
     // Summary:
     //     the command/script that was executed in order to create the layer.
+    [JsonPropertyName("createdBy")]
     public string CreatedBy { get; set; }
 
     // Summary:
     // The Layer hash (docker inspect) that represents the changes between this layer and the previous layer
+    [JsonPropertyName("diffId")]
     public string DiffId { get; set; }
 
     // Summary:
     //     Whether or not this layer was found in the base image of the container
+    [JsonPropertyName("isBaseImage")]
     public bool IsBaseImage { get; set; }
 
     // Summary:
     //     0-indexed monotonically increasing ID for the order of the layer in the container.
     //     Note: only includes non-empty layers
+    [JsonPropertyName("layerIndex")]
     public int LayerIndex { get; set; }
 }
