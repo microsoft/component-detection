f1cda808283533a4a3e6b5584113a6f58e2b48f5
Youssef Victor <youssefvictor00@gmail.com>
2025-12-03 13:53:36 +0100

    Update CommandLineInvocationServiceTests.cs

--- test/Microsoft.ComponentDetection.Common.Tests/CommandLineInvocationServiceTests.cs
+++ test/Microsoft.ComponentDetection.Common.Tests/CommandLineInvocationServiceTests.cs
@@ -6,7 +6,6 @@ using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using AwesomeAssertions;
-using Microsoft.ComponentDetection.TestsUtilities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 [TestClass]
