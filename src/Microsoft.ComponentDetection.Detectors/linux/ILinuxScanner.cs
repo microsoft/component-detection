dd489e1597fc5d079dfeed1965115a53240675c0
Zheng Hao Tang <zhenghao104@gmail.com>
2026-01-14 12:51:51 -0800

    Revert "Add image scope scanning option to the Linux detector (#1600)"
    This reverts commit c6846ef93e43f42c2ef97e58873d04d6bb8625b2.

--- src/Microsoft.ComponentDetection.Detectors/linux/ILinuxScanner.cs
+++ src/Microsoft.ComponentDetection.Detectors/linux/ILinuxScanner.cs
@@ -19,7 +19,6 @@ public interface ILinuxScanner
     /// <param name="containerLayers">The collection of Docker layers that make up the container image.</param>
     /// <param name="baseImageLayerCount">The number of layers that belong to the base image, used to distinguish base image layers from application layers.</param>
     /// <param name="enabledComponentTypes">The set of component types to include in the scan results. Only components matching these types will be returned.</param>
-    /// <param name="scope">The scope for scanning the image. See <see cref="LinuxScannerScope"/> for values.</param>
     /// <param name="cancellationToken">A token to monitor for cancellation requests. The default value is <see cref="CancellationToken.None"/>.</param>
     /// <returns>A task that represents the asynchronous operation. The task result contains a collection of <see cref="LayerMappedLinuxComponents"/> representing the components found in the image and their associated layers.</returns>
     public Task<IEnumerable<LayerMappedLinuxComponents>> ScanLinuxAsync(
@@ -27,7 +26,6 @@ public interface ILinuxScanner
         IEnumerable<DockerLayer> containerLayers,
         int baseImageLayerCount,
         ISet<ComponentType> enabledComponentTypes,
-        LinuxScannerScope scope,
         CancellationToken cancellationToken = default
     );
 }
