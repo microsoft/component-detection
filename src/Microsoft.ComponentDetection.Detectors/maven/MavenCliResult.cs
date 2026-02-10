5e48830ae60c57f6148d91b6c00b6a7aadf13690
Zheng Hao Tang <zhentan@microsoft.com>
2026-02-09 16:18:22 -0800

    Remove unused parameter

--- src/Microsoft.ComponentDetection.Detectors/maven/MavenCliResult.cs
+++ src/Microsoft.ComponentDetection.Detectors/maven/MavenCliResult.cs
@@ -5,5 +5,4 @@ namespace Microsoft.ComponentDetection.Detectors.Maven;
 /// </summary>
 /// <param name="Success">Whether the command completed successfully.</param>
 /// <param name="ErrorOutput">The error output from the command, if any.</param>
-/// <param name="DependenciesFileContent">The content of the generated dependencies file, if successful.</param>
-public record MavenCliResult(bool Success, string ErrorOutput, string? DependenciesFileContent = null);
+public record MavenCliResult(bool Success, string? ErrorOutput);
