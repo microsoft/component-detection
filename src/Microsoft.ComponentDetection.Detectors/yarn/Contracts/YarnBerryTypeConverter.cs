856f26c1ca0086f32807c969d30c68514788da64
Jamie Magee <jamie.magee@gmail.com>
2026-02-18 21:25:02 -0800

    Fix YarnBerryTypeConverter for YamlDotNet 16 interface changes
    YamlDotNet 16 changed IYamlTypeConverter: ReadYaml now takes an
    ObjectDeserializer parameter, WriteYaml now takes an ObjectSerializer
    and the value is nullable. Updated both method signatures to match.
    
    Removed #nullable disable since the interface requires object?.

--- src/Microsoft.ComponentDetection.Detectors/yarn/Contracts/YarnBerryTypeConverter.cs
+++ src/Microsoft.ComponentDetection.Detectors/yarn/Contracts/YarnBerryTypeConverter.cs
@@ -1,4 +1,3 @@
-#nullable disable
 namespace Microsoft.ComponentDetection.Detectors.Yarn.Contracts;
 
 using System;
@@ -16,7 +15,7 @@ public class YarnBerryTypeConverter : IYamlTypeConverter
     public bool Accepts(Type type) => type == typeof(YarnBerryLockfile);
 
     /// <inheritdoc />
-    public object ReadYaml(IParser parser, Type type)
+    public object ReadYaml(IParser parser, Type type, ObjectDeserializer rootDeserializer)
     {
         var metadata = new YarnBerryLockfileMetadata();
         var entries = new Dictionary<string, YarnBerryLockfileEntry>();
@@ -44,13 +43,14 @@ public class YarnBerryTypeConverter : IYamlTypeConverter
             }
         }
 
-        return new YarnBerryLockfile
-        {
-            Metadata = metadata,
-            Entries = entries,
-        };
+        return new YarnBerryLockfile { Metadata = metadata, Entries = entries };
     }
 
     /// <inheritdoc />
-    public void WriteYaml(IEmitter emitter, object value, Type type) => throw new NotImplementedException();
+    public void WriteYaml(
+        IEmitter emitter,
+        object? value,
+        Type type,
+        ObjectSerializer serializer
+    ) => throw new NotImplementedException();
 }
