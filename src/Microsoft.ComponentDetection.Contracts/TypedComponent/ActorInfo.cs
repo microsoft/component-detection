258d5c2a305d1c414357141746abf937711607dd
Paul Dorsch <pauldorsch@microsoft.com>
2026-03-03 10:34:38 -0800

    update actor info

--- src/Microsoft.ComponentDetection.Contracts/TypedComponent/ActorInfo.cs
+++ src/Microsoft.ComponentDetection.Contracts/TypedComponent/ActorInfo.cs
@@ -1,31 +1,38 @@
 namespace Microsoft.ComponentDetection.Contracts.TypedComponent;
 
 using System;
-using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using SystemTextJson = System.Text.Json.Serialization;
 
 /// <summary>
 /// Represents an actor (person, organization, or software agent) involved with a component.
 /// At least one of <see cref="Name"/>, <see cref="Email"/>, or <see cref="Url"/> should be populated.
 /// Aligned with SPDX 3.0.1 Agent subclasses.
 /// </summary>
+[JsonObject(MemberSerialization.OptOut, NamingStrategyType = typeof(CamelCaseNamingStrategy))]
 public class ActorInfo
 {
-    [JsonPropertyName("name")]
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [SystemTextJson.JsonPropertyName("name")]
+    [SystemTextJson.JsonIgnore(Condition = SystemTextJson.JsonIgnoreCondition.WhenWritingNull)]
+    [JsonProperty("name", NullValueHandling = NullValueHandling.Ignore)]
     public string? Name { get; set; }
 
-    [JsonPropertyName("email")]
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [SystemTextJson.JsonPropertyName("email")]
+    [SystemTextJson.JsonIgnore(Condition = SystemTextJson.JsonIgnoreCondition.WhenWritingNull)]
+    [JsonProperty("email", NullValueHandling = NullValueHandling.Ignore)]
     public string? Email { get; set; }
 
-    [JsonPropertyName("url")]
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [SystemTextJson.JsonPropertyName("url")]
+    [SystemTextJson.JsonIgnore(Condition = SystemTextJson.JsonIgnoreCondition.WhenWritingNull)]
+    [JsonProperty("url", NullValueHandling = NullValueHandling.Ignore)]
     public Uri? Url { get; set; }
 
     /// <summary>
     /// The type of actor. One of: "Person", "Organization", or "SoftwareAgent".
     /// </summary>
-    [JsonPropertyName("type")]
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [SystemTextJson.JsonPropertyName("type")]
+    [SystemTextJson.JsonIgnore(Condition = SystemTextJson.JsonIgnoreCondition.WhenWritingNull)]
+    [JsonProperty("type", NullValueHandling = NullValueHandling.Ignore)]
     public string? Type { get; set; }
 }
