ceb93dacc18dc2c382b3959836acd52ba95fb2ab
Jamie Magee <jamie.magee@gmail.com>
2026-03-05 21:06:41 -0800

    refactor: make Orchestrator implementation details internal
    Make 8 classes internal in the Orchestrator project:
    
    - Converter classes: CommaDelimitedConverter, KeyValueDelimitedConverter,
      SemicolonDelimitedConverter
    - CLI helpers: ArgumentHelper, IArgumentHelper
    - Concrete service implementations: DetectorProcessingService,
      DetectorRestrictionService, DefaultGraphTranslationService
    
    Kept public: ServiceCollectionExtensions, ScanExecutionService, ScanCommand,
    ScanSettings, LoggingEnricher, DetectorRestrictions, all experiment types,
    all service interfaces, DetectorRunResult, DetectorProcessingResult.
    
    Part of #455

--- src/Microsoft.ComponentDetection.Orchestrator/Extensions/SemicolonDelimitedConverter.cs
+++ src/Microsoft.ComponentDetection.Orchestrator/Extensions/SemicolonDelimitedConverter.cs
@@ -8,7 +8,7 @@ using System.Globalization;
 /// <summary>
 /// Converts a semicolon separated string to an array of strings.
 /// </summary>
-public class SemicolonDelimitedConverter : TypeConverter
+internal class SemicolonDelimitedConverter : TypeConverter
 {
     /// <inheritdoc />
     public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
