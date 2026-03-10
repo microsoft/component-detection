725717bfbf3aa44113ab8ca1164b85302c6e5c19
Zheng Hao Tang <zhentan@microsoft.com>
2026-03-10 12:06:17 -0700

    Fix submodule bug in MavenFallbackDetector (#1685)
    * Fix submodule bug in MavenFallbackDetector
    
    * Optimizations for file enumeration
    
    * Make results a queue to maintain parent ordering
    
    * Add unit test to ensure ordering
    
    * Version bump
    
    * Fix test failure
    
    * Semaphore logic to safeguard file deletion
    
    * PR comment
    
    * Nit cleanup
    
    ---------
    
    Co-authored-by: Fernando Rojo <ferojo@microsoft.com>

--- src/Microsoft.ComponentDetection.Detectors/maven/IMavenCommandService.cs
+++ src/Microsoft.ComponentDetection.Detectors/maven/IMavenCommandService.cs
@@ -14,4 +14,18 @@ public interface IMavenCommandService
     Task<MavenCliResult> GenerateDependenciesFileAsync(ProcessRequest processRequest, CancellationToken cancellationToken = default);
 
     void ParseDependenciesFile(ProcessRequest processRequest);
+
+    /// <summary>
+    /// Registers that a detector is actively reading a dependency file.
+    /// This prevents premature deletion by other detectors.
+    /// </summary>
+    /// <param name="dependencyFilePath">The path to the dependency file being read.</param>
+    void RegisterFileReader(string dependencyFilePath);
+
+    /// <summary>
+    /// Unregisters a detector's active reading of a dependency file and attempts cleanup.
+    /// If no other detectors are reading the file, it will be safely deleted.
+    /// </summary>
+    /// <param name="dependencyFilePath">The path to the dependency file that was being read.</param>
+    void UnregisterFileReader(string dependencyFilePath);
 }
