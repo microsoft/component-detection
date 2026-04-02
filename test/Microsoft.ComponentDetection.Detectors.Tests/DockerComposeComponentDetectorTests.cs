077d1d08a4d207e38ffc570e4d075389bda4a5b5
Julian Pinzer <jupinzer@microsoft.com>
2026-04-02 17:36:43 -0400

    Add tests for Docker Compose, Dockerfile, Helm, and Kubernetes component detectors

---
+++ test/Microsoft.ComponentDetection.Detectors.Tests/DockerComposeComponentDetectorTests.cs
@@ -0,0 +1,192 @@
+namespace Microsoft.ComponentDetection.Detectors.Tests;
+
+using System.Linq;
+using System.Threading.Tasks;
+using AwesomeAssertions;
+using Microsoft.ComponentDetection.Contracts;
+using Microsoft.ComponentDetection.Contracts.TypedComponent;
+using Microsoft.ComponentDetection.Detectors.DockerCompose;
+using Microsoft.ComponentDetection.TestsUtilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+[TestCategory("Governance/All")]
+[TestCategory("Governance/ComponentDetection")]
+public class DockerComposeComponentDetectorTests : BaseDetectorTest<DockerComposeComponentDetector>
+{
+    [TestMethod]
+    public async Task TestCompose_SingleServiceImageAsync()
+    {
+        var composeYaml = @"
+version: '3'
+services:
+  web:
+    image: nginx:1.21
+    ports:
+      - ""80:80""
+";
+
+        var (scanResult, componentRecorder) = await this.DetectorTestUtility
+            .WithFile("docker-compose.yml", composeYaml)
+            .ExecuteDetectorAsync();
+
+        scanResult.ResultCode.Should().Be(ProcessingResultCode.Success);
+        var components = componentRecorder.GetDetectedComponents();
+        components.Should().ContainSingle();
+
+        var dockerRef = components.First().Component as DockerReferenceComponent;
+        dockerRef.Should().NotBeNull();
+        dockerRef.Repository.Should().Be("library/nginx");
+        dockerRef.Tag.Should().Be("1.21");
+    }
+
+    [TestMethod]
+    public async Task TestCompose_MultipleServicesAsync()
+    {
+        var composeYaml = @"
+version: '3'
+services:
+  web:
+    image: nginx:1.21
+  db:
+    image: postgres:15
+  cache:
+    image: redis:7-alpine
+";
+
+        var (scanResult, componentRecorder) = await this.DetectorTestUtility
+            .WithFile("docker-compose.yml", composeYaml)
+            .ExecuteDetectorAsync();
+
+        scanResult.ResultCode.Should().Be(ProcessingResultCode.Success);
+        var components = componentRecorder.GetDetectedComponents();
+        components.Should().HaveCount(3);
+    }
+
+    [TestMethod]
+    public async Task TestCompose_FullRegistryImageAsync()
+    {
+        var composeYaml = @"
+services:
+  app:
+    image: ghcr.io/myorg/myapp:v2.0
+";
+
+        var (scanResult, componentRecorder) = await this.DetectorTestUtility
+            .WithFile("compose.yaml", composeYaml)
+            .ExecuteDetectorAsync();
+
+        scanResult.ResultCode.Should().Be(ProcessingResultCode.Success);
+        var components = componentRecorder.GetDetectedComponents();
+        components.Should().ContainSingle();
+
+        var dockerRef = components.First().Component as DockerReferenceComponent;
+        dockerRef.Should().NotBeNull();
+        dockerRef.Domain.Should().Be("ghcr.io");
+        dockerRef.Repository.Should().Be("myorg/myapp");
+        dockerRef.Tag.Should().Be("v2.0");
+    }
+
+    [TestMethod]
+    public async Task TestCompose_BuildOnlyServiceIgnoredAsync()
+    {
+        var composeYaml = @"
+version: '3'
+services:
+  app:
+    build: ./app
+    ports:
+      - ""3000:3000""
+";
+
+        var (scanResult, componentRecorder) = await this.DetectorTestUtility
+            .WithFile("docker-compose.yml", composeYaml)
+            .ExecuteDetectorAsync();
+
+        scanResult.ResultCode.Should().Be(ProcessingResultCode.Success);
+        componentRecorder.GetDetectedComponents().Should().BeEmpty();
+    }
+
+    [TestMethod]
+    public async Task TestCompose_MixedBuildAndImageAsync()
+    {
+        var composeYaml = @"
+version: '3'
+services:
+  app:
+    build: ./app
+  db:
+    image: postgres:15
+";
+
+        var (scanResult, componentRecorder) = await this.DetectorTestUtility
+            .WithFile("docker-compose.yml", composeYaml)
+            .ExecuteDetectorAsync();
+
+        scanResult.ResultCode.Should().Be(ProcessingResultCode.Success);
+        var components = componentRecorder.GetDetectedComponents();
+        components.Should().ContainSingle();
+
+        var dockerRef = components.First().Component as DockerReferenceComponent;
+        dockerRef.Should().NotBeNull();
+        dockerRef.Repository.Should().Be("library/postgres");
+        dockerRef.Tag.Should().Be("15");
+    }
+
+    [TestMethod]
+    public async Task TestCompose_NoServicesKeyAsync()
+    {
+        var composeYaml = @"
+version: '3'
+networks:
+  frontend:
+";
+
+        var (scanResult, componentRecorder) = await this.DetectorTestUtility
+            .WithFile("docker-compose.yml", composeYaml)
+            .ExecuteDetectorAsync();
+
+        scanResult.ResultCode.Should().Be(ProcessingResultCode.Success);
+        componentRecorder.GetDetectedComponents().Should().BeEmpty();
+    }
+
+    [TestMethod]
+    public async Task TestCompose_ImageWithDigestAsync()
+    {
+        var composeYaml = @"
+services:
+  app:
+    image: nginx@sha256:abc123def456abc123def456abc123def456abc123def456abc123def456abc1
+";
+
+        var (scanResult, componentRecorder) = await this.DetectorTestUtility
+            .WithFile("docker-compose.yaml", composeYaml)
+            .ExecuteDetectorAsync();
+
+        scanResult.ResultCode.Should().Be(ProcessingResultCode.Success);
+        var components = componentRecorder.GetDetectedComponents();
+        components.Should().ContainSingle();
+
+        var dockerRef = components.First().Component as DockerReferenceComponent;
+        dockerRef.Should().NotBeNull();
+        dockerRef.Digest.Should().Be("sha256:abc123def456abc123def456abc123def456abc123def456abc123def456abc1");
+    }
+
+    [TestMethod]
+    public async Task TestCompose_OverrideFileAsync()
+    {
+        var composeYaml = @"
+services:
+  web:
+    image: myregistry.io/web:latest
+";
+
+        var (scanResult, componentRecorder) = await this.DetectorTestUtility
+            .WithFile("docker-compose.override.yml", composeYaml)
+            .ExecuteDetectorAsync();
+
+        scanResult.ResultCode.Should().Be(ProcessingResultCode.Success);
+        var components = componentRecorder.GetDetectedComponents();
+        components.Should().ContainSingle();
+    }
+}
---
+++ test/Microsoft.ComponentDetection.Detectors.Tests/HelmComponentDetectorTests.cs
@@ -0,0 +1,194 @@
+namespace Microsoft.ComponentDetection.Detectors.Tests;
+
+using System.Linq;
+using System.Threading.Tasks;
+using AwesomeAssertions;
+using Microsoft.ComponentDetection.Contracts;
+using Microsoft.ComponentDetection.Contracts.TypedComponent;
+using Microsoft.ComponentDetection.Detectors.Helm;
+using Microsoft.ComponentDetection.TestsUtilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+[TestCategory("Governance/All")]
+[TestCategory("Governance/ComponentDetection")]
+public class HelmComponentDetectorTests : BaseDetectorTest<HelmComponentDetector>
+{
+    [TestMethod]
+    public async Task TestHelm_DirectImageStringAsync()
+    {
+        var valuesYaml = @"
+replicaCount: 1
+image: nginx:1.21
+service:
+  type: ClusterIP
+";
+
+        var (scanResult, componentRecorder) = await this.DetectorTestUtility
+            .WithFile("values.yaml", valuesYaml)
+            .ExecuteDetectorAsync();
+
+        scanResult.ResultCode.Should().Be(ProcessingResultCode.Success);
+        var components = componentRecorder.GetDetectedComponents();
+        components.Should().ContainSingle();
+
+        var dockerRef = components.First().Component as DockerReferenceComponent;
+        dockerRef.Should().NotBeNull();
+        dockerRef.Repository.Should().Be("library/nginx");
+        dockerRef.Tag.Should().Be("1.21");
+    }
+
+    [TestMethod]
+    public async Task TestHelm_StructuredImageReferenceAsync()
+    {
+        var valuesYaml = @"
+replicaCount: 1
+image:
+  repository: myregistry.io/myapp
+  tag: ""2.0.0""
+service:
+  type: ClusterIP
+";
+
+        var (scanResult, componentRecorder) = await this.DetectorTestUtility
+            .WithFile("values.yaml", valuesYaml)
+            .ExecuteDetectorAsync();
+
+        scanResult.ResultCode.Should().Be(ProcessingResultCode.Success);
+        var components = componentRecorder.GetDetectedComponents();
+        components.Should().ContainSingle();
+
+        var dockerRef = components.First().Component as DockerReferenceComponent;
+        dockerRef.Should().NotBeNull();
+        dockerRef.Repository.Should().Be("myapp");
+        dockerRef.Domain.Should().Be("myregistry.io");
+        dockerRef.Tag.Should().Be("2.0.0");
+    }
+
+    [TestMethod]
+    public async Task TestHelm_StructuredImageWithRegistryAsync()
+    {
+        var valuesYaml = @"
+image:
+  registry: ghcr.io
+  repository: org/myimage
+  tag: ""v1.0""
+";
+
+        var (scanResult, componentRecorder) = await this.DetectorTestUtility
+            .WithFile("values.yaml", valuesYaml)
+            .ExecuteDetectorAsync();
+
+        scanResult.ResultCode.Should().Be(ProcessingResultCode.Success);
+        var components = componentRecorder.GetDetectedComponents();
+        components.Should().ContainSingle();
+
+        var dockerRef = components.First().Component as DockerReferenceComponent;
+        dockerRef.Should().NotBeNull();
+        dockerRef.Domain.Should().Be("ghcr.io");
+        dockerRef.Repository.Should().Be("org/myimage");
+        dockerRef.Tag.Should().Be("v1.0");
+    }
+
+    [TestMethod]
+    public async Task TestHelm_NestedImageReferencesAsync()
+    {
+        var valuesYaml = @"
+app:
+  frontend:
+    image: nginx:1.21
+  backend:
+    image:
+      repository: node
+      tag: ""18-alpine""
+";
+
+        var (scanResult, componentRecorder) = await this.DetectorTestUtility
+            .WithFile("values.yaml", valuesYaml)
+            .ExecuteDetectorAsync();
+
+        scanResult.ResultCode.Should().Be(ProcessingResultCode.Success);
+        var components = componentRecorder.GetDetectedComponents();
+        components.Should().HaveCount(2);
+    }
+
+    [TestMethod]
+    public async Task TestHelm_EmptyValuesYamlAsync()
+    {
+        var valuesYaml = @"
+replicaCount: 1
+service:
+  type: ClusterIP
+";
+
+        var (scanResult, componentRecorder) = await this.DetectorTestUtility
+            .WithFile("values.yaml", valuesYaml)
+            .ExecuteDetectorAsync();
+
+        scanResult.ResultCode.Should().Be(ProcessingResultCode.Success);
+        componentRecorder.GetDetectedComponents().Should().BeEmpty();
+    }
+
+    [TestMethod]
+    public async Task TestHelm_ChartYamlIgnoredAsync()
+    {
+        var chartYaml = @"
+apiVersion: v2
+name: my-chart
+version: 0.1.0
+dependencies:
+  - name: postgresql
+    version: ""11.0.0""
+    repository: https://charts.bitnami.com/bitnami
+";
+
+        var (scanResult, componentRecorder) = await this.DetectorTestUtility
+            .WithFile("Chart.yaml", chartYaml)
+            .ExecuteDetectorAsync();
+
+        scanResult.ResultCode.Should().Be(ProcessingResultCode.Success);
+        componentRecorder.GetDetectedComponents().Should().BeEmpty();
+    }
+
+    [TestMethod]
+    public async Task TestHelm_ImageWithDigestAsync()
+    {
+        var valuesYaml = @"
+image:
+  repository: nginx
+  digest: ""sha256:abc123def456abc123def456abc123def456abc123def456abc123def456abc1""
+";
+
+        var (scanResult, componentRecorder) = await this.DetectorTestUtility
+            .WithFile("values.yaml", valuesYaml)
+            .ExecuteDetectorAsync();
+
+        scanResult.ResultCode.Should().Be(ProcessingResultCode.Success);
+        var components = componentRecorder.GetDetectedComponents();
+        components.Should().ContainSingle();
+
+        var dockerRef = components.First().Component as DockerReferenceComponent;
+        dockerRef.Should().NotBeNull();
+        dockerRef.Digest.Should().Be("sha256:abc123def456abc123def456abc123def456abc123def456abc123def456abc1");
+    }
+
+    [TestMethod]
+    public async Task TestHelm_ImagesInSequenceAsync()
+    {
+        var valuesYaml = @"
+sidecars:
+  - name: sidecar1
+    image: busybox:1.35
+  - name: sidecar2
+    image: alpine:3.18
+";
+
+        var (scanResult, componentRecorder) = await this.DetectorTestUtility
+            .WithFile("values.yaml", valuesYaml)
+            .ExecuteDetectorAsync();
+
+        scanResult.ResultCode.Should().Be(ProcessingResultCode.Success);
+        var components = componentRecorder.GetDetectedComponents();
+        components.Should().HaveCount(2);
+    }
+}
