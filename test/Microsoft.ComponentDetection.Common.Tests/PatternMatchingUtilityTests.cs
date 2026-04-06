8547c2c6d448f46518071e0bab8b3f3f1c875111
Jamie Magee <jamie.magee@gmail.com>
2026-04-06 12:35:56 -0700

    Replace expression tree pattern matching with closures

--- test/Microsoft.ComponentDetection.Common.Tests/PatternMatchingUtilityTests.cs
+++ test/Microsoft.ComponentDetection.Common.Tests/PatternMatchingUtilityTests.cs
@@ -10,38 +10,25 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PatternMatchingUtilityTests
 {
     [TestMethod]
-    public void PatternMatcher_Matches_StartsWith()
+    [DataRow("test*", "test123", true)]
+    [DataRow("test*", "123test", false)]
+    [DataRow("*test", "123test", true)]
+    [DataRow("*test", "test123", false)]
+    [DataRow("test", "test", true)]
+    [DataRow("test", "123test", false)]
+    [DataRow("*test*", "123test456", true)]
+    [DataRow("*test*", "test456", true)]
+    [DataRow("*test*", "123test", true)]
+    [DataRow("*test*", "test", true)]
+    [DataRow("*test*", "tes", false)]
+    [DataRow("*", "anything", true)]
+    [DataRow("*", "", true)]
+    [DataRow("**", "anything", true)]
+    [DataRow("**", "", true)]
+    public void PatternMatcher_MatchesExpected(string pattern, string input, bool expected)
     {
-        var pattern = "test*";
-        var input = "test123";
-
         var matcher = PatternMatchingUtility.GetFilePatternMatcher([pattern]);
 
-        matcher(input).Should().BeTrue();
-        matcher("123test").Should().BeFalse();
-    }
-
-    [TestMethod]
-    public void PatternMatcher_Matches_EndsWith()
-    {
-        var pattern = "*test";
-        var input = "123test";
-
-        var matcher = PatternMatchingUtility.GetFilePatternMatcher([pattern]);
-
-        matcher(input).Should().BeTrue();
-        matcher("test123").Should().BeFalse();
-    }
-
-    [TestMethod]
-    public void PatternMatcher_Matches_Exact()
-    {
-        var pattern = "test";
-        var input = "test";
-
-        var matcher = PatternMatchingUtility.GetFilePatternMatcher([pattern]);
-
-        matcher(input).Should().BeTrue();
-        matcher("123test").Should().BeFalse();
+        matcher(input).Should().Be(expected);
     }
 }
