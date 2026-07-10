60535929ae4a65c3b77b3dc02b10aa0e2f93f3c2
Fernando Rojo <ferojo@microsoft.com>
2026-07-09 20:34:55 -0700

    Update tests

--- test/Microsoft.ComponentDetection.Common.Tests/BaseDetectionTelemetryRecordTests.cs
+++ test/Microsoft.ComponentDetection.Common.Tests/BaseDetectionTelemetryRecordTests.cs
@@ -7,9 +7,11 @@ using System.Linq;
 using System.Net;
 using System.Reflection;
 using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
 using AwesomeAssertions;
 using Microsoft.ComponentDetection.Common.Telemetry;
 using Microsoft.ComponentDetection.Common.Telemetry.Records;
+using Microsoft.ComponentDetection.Contracts;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 
@@ -19,6 +21,9 @@ using Moq;
 public class BaseDetectionTelemetryRecordTests
 {
     private Type[] recordTypes;
+    private bool originalDiagnosticEnabled;
+    private Mock<ITelemetryService> telemetryServiceMock;
+    private List<IDetectionTelemetryRecord> publishedRecords;
 
     [TestInitialize]
     public void Initialize()
@@ -28,6 +33,23 @@ public class BaseDetectionTelemetryRecordTests
             .Where(type => typeof(BaseDetectionTelemetryRecord).IsAssignableFrom(type))
             .Where(type => !type.IsAbstract)
             .ToArray();
+
+        this.originalDiagnosticEnabled = BaseDetectionTelemetryRecord.DiagnosticEnabled;
+
+        this.publishedRecords = [];
+        this.telemetryServiceMock = new Mock<ITelemetryService>();
+        this.telemetryServiceMock
+            .Setup(x => x.PostRecord(It.IsAny<IDetectionTelemetryRecord>()))
+            .Callback<IDetectionTelemetryRecord>(this.publishedRecords.Add);
+
+        TelemetryRelay.Instance.Init([this.telemetryServiceMock.Object]);
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+        BaseDetectionTelemetryRecord.DiagnosticEnabled = this.originalDiagnosticEnabled;
+        TelemetryRelay.Instance.Init([]);
     }
 
     [TestMethod]
@@ -68,6 +90,11 @@ public class BaseDetectionTelemetryRecordTests
         {
             foreach (var property in type.GetProperties(BindingFlags.Public | BindingFlags.Instance))
             {
+                if (property.GetCustomAttribute<JsonIgnoreAttribute>() != null)
+                {
+                    continue;
+                }
+
                 if (!serializableTypes.Contains(property.PropertyType))
                 {
                     Attribute.GetCustomAttribute(property.PropertyType, typeof(DataContractAttribute)).Should().NotBeNull(
@@ -79,63 +106,163 @@ public class BaseDetectionTelemetryRecordTests
     }
 
     [TestMethod]
-    public void NonDiagnosticRecord_IsAlwaysPosted_WhenDisposed()
+    public void IsDiagnostic_DefaultsToFalse()
     {
-        var telemetryServiceMock = new Mock<ITelemetryService>();
-        var postedRecords = new List<IDetectionTelemetryRecord>();
-        telemetryServiceMock.Setup(x => x.PostRecord(It.IsAny<IDetectionTelemetryRecord>()))
-            .Callback<IDetectionTelemetryRecord>(postedRecords.Add);
-        TelemetryRelay.Instance.Init([telemetryServiceMock.Object]);
-
-        try
-        {
-            // Disposal triggers the telemetry posting in BaseDetectionTelemetryRecord.Dispose()
-            using (new NonDiagnosticTestRecord())
-            {
-            }
-
-            postedRecords.Should().ContainSingle();
-        }
-        finally
-        {
-            TelemetryRelay.Instance.Init([]);
-        }
+        new CommandLineInvocationTelemetryRecord().IsDiagnostic.Should().BeFalse();
+        new DetectorExecutionTelemetryRecord().IsDiagnostic.Should().BeFalse();
     }
 
     [TestMethod]
-    public void DiagnosticRecord_PostingIsGatedOnDiagnosticsFlag()
+    public void IsDiagnostic_HasJsonIgnoreAttribute()
     {
-        var telemetryServiceMock = new Mock<ITelemetryService>();
-        var postedRecords = new List<IDetectionTelemetryRecord>();
-        telemetryServiceMock.Setup(x => x.PostRecord(It.IsAny<IDetectionTelemetryRecord>()))
-            .Callback<IDetectionTelemetryRecord>(postedRecords.Add);
-        TelemetryRelay.Instance.Init([telemetryServiceMock.Object]);
+        var property = typeof(BaseDetectionTelemetryRecord).GetProperty(nameof(BaseDetectionTelemetryRecord.IsDiagnostic));
 
-        try
-        {
-            // Disposal triggers the telemetry posting in BaseDetectionTelemetryRecord.Dispose()
-            using (new DiagnosticTestRecord())
-            {
-            }
-
-            var diagnosticEnabled = (bool)typeof(BaseDetectionTelemetryRecord)
-                .GetField("DiagnosticEnabled", BindingFlags.NonPublic | BindingFlags.Static)
-                .GetValue(null);
-
-            if (diagnosticEnabled)
-            {
-                postedRecords.Should().ContainSingle("diagnostic records should be posted when diagnostics are enabled");
-            }
-            else
-            {
-                postedRecords.Should().BeEmpty("diagnostic records should be suppressed when diagnostics are disabled");
-            }
-        }
-        finally
-        {
-            TelemetryRelay.Instance.Init([]);
-        }
+        property.GetCustomAttribute<JsonIgnoreAttribute>().Should().NotBeNull(
+            "IsDiagnostic should not be serialized into telemetry output");
     }
+
+    [TestMethod]
+    public void NonDiagnosticRecord_IsPosted_WhenDiagnosticsDisabled()
+    {
+        BaseDetectionTelemetryRecord.DiagnosticEnabled = false;
+
+        using (new NonDiagnosticTestRecord())
+        {
+        }
+
+        this.publishedRecords.Should().ContainSingle("regular records are always published regardless of DiagnosticEnabled");
+    }
+
+    [TestMethod]
+    public void NonDiagnosticRecord_IsPosted_WhenDiagnosticsEnabled()
+    {
+        BaseDetectionTelemetryRecord.DiagnosticEnabled = true;
+
+        using (new NonDiagnosticTestRecord())
+        {
+        }
+
+        this.publishedRecords.Should().ContainSingle("regular records are always published regardless of DiagnosticEnabled");
+    }
+
+    [TestMethod]
+    public void DiagnosticRecord_NotPosted_WhenDiagnosticsDisabled()
+    {
+        BaseDetectionTelemetryRecord.DiagnosticEnabled = false;
+
+        using (new DiagnosticTestRecord())
+        {
+        }
+
+        this.publishedRecords.Should().BeEmpty("diagnostic records should not be published when DiagnosticEnabled=false");
+    }
+
+    [TestMethod]
+    public void DiagnosticRecord_IsPosted_WhenDiagnosticsEnabled()
+    {
+        BaseDetectionTelemetryRecord.DiagnosticEnabled = true;
+
+        using (new DiagnosticTestRecord())
+        {
+        }
+
+        this.publishedRecords.Should().ContainSingle("diagnostic records should be published when DiagnosticEnabled=true");
+    }
+
+    [TestMethod]
+    public void CommandLineInvocation_StandardError_TruncatedTo10Lines_WhenDiagnosticsDisabled()
+    {
+        BaseDetectionTelemetryRecord.DiagnosticEnabled = false;
+
+        var record = new CommandLineInvocationTelemetryRecord();
+        record.Track(new CommandLineExecutionResult { ExitCode = 1, StdErr = this.BuildLines(50) }, "/cmd", string.Empty);
+
+        record.StandardError.Split(Environment.NewLine).Should().HaveCount(10);
+        record.StandardError.Should().Contain("Line 1");
+        record.StandardError.Should().Contain("Line 10");
+        record.StandardError.Should().NotContain("Line 11");
+    }
+
+    [TestMethod]
+    public void CommandLineInvocation_StandardError_NotTruncated_WhenDiagnosticsEnabled()
+    {
+        BaseDetectionTelemetryRecord.DiagnosticEnabled = true;
+
+        var record = new CommandLineInvocationTelemetryRecord();
+        record.Track(new CommandLineExecutionResult { ExitCode = 1, StdErr = this.BuildLines(50) }, "/cmd", string.Empty);
+
+        record.StandardError.Split(Environment.NewLine).Should().HaveCount(50);
+    }
+
+    [TestMethod]
+    public void CommandLineInvocation_StandardError_Unchanged_WhenUnder10Lines()
+    {
+        BaseDetectionTelemetryRecord.DiagnosticEnabled = false;
+
+        var fiveLines = this.BuildLines(5);
+        var record = new CommandLineInvocationTelemetryRecord();
+        record.Track(new CommandLineExecutionResult { ExitCode = 1, StdErr = fiveLines }, "/cmd", string.Empty);
+
+        record.StandardError.Should().Be(fiveLines);
+    }
+
+    [TestMethod]
+    public void CommandLineInvocation_StandardError_NullHandled()
+    {
+        BaseDetectionTelemetryRecord.DiagnosticEnabled = false;
+
+        var record = new CommandLineInvocationTelemetryRecord();
+        record.Track(new CommandLineExecutionResult { ExitCode = 0, StdErr = null }, "/cmd", string.Empty);
+
+        record.StandardError.Should().BeNull();
+    }
+
+    [TestMethod]
+    public void DetectorExecution_ExperimentalInformation_TruncatedTo10Lines_WhenDiagnosticsDisabled()
+    {
+        BaseDetectionTelemetryRecord.DiagnosticEnabled = false;
+
+        var record = new DetectorExecutionTelemetryRecord { ExperimentalInformation = this.BuildLines(50) };
+
+        record.ExperimentalInformation.Split(Environment.NewLine).Should().HaveCount(10);
+        record.ExperimentalInformation.Should().Contain("Line 1");
+        record.ExperimentalInformation.Should().Contain("Line 10");
+        record.ExperimentalInformation.Should().NotContain("Line 11");
+    }
+
+    [TestMethod]
+    public void DetectorExecution_ExperimentalInformation_NotTruncated_WhenDiagnosticsEnabled()
+    {
+        BaseDetectionTelemetryRecord.DiagnosticEnabled = true;
+
+        var record = new DetectorExecutionTelemetryRecord { ExperimentalInformation = this.BuildLines(50) };
+
+        record.ExperimentalInformation.Split(Environment.NewLine).Should().HaveCount(50);
+    }
+
+    [TestMethod]
+    public void DetectorExecution_ExperimentalInformation_Unchanged_WhenUnder10Lines()
+    {
+        BaseDetectionTelemetryRecord.DiagnosticEnabled = false;
+
+        var fiveLines = this.BuildLines(5);
+        var record = new DetectorExecutionTelemetryRecord { ExperimentalInformation = fiveLines };
+
+        record.ExperimentalInformation.Should().Be(fiveLines);
+    }
+
+    [TestMethod]
+    public void DetectorExecution_ExperimentalInformation_NullHandled()
+    {
+        BaseDetectionTelemetryRecord.DiagnosticEnabled = false;
+
+        var record = new DetectorExecutionTelemetryRecord { ExperimentalInformation = null };
+
+        record.ExperimentalInformation.Should().BeNull();
+    }
+
+    private string BuildLines(int count) =>
+        string.Join(Environment.NewLine, Enumerable.Range(1, count).Select(i => $"Line {i}"));
 
     private sealed class NonDiagnosticTestRecord : BaseDetectionTelemetryRecord
     {
