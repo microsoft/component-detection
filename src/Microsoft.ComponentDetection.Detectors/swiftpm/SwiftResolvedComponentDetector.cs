0de0de9f14a96e0f3da032b1818f3f4df9444814
Ryan Brandenburg (He/Him) <rybrande@microsoft.com>
2026-09-17 11:44:36 -0700

    Emit SwiftComponent afterall

--- src/Microsoft.ComponentDetection.Detectors/swiftpm/SwiftResolvedComponentDetector.cs
+++ src/Microsoft.ComponentDetection.Detectors/swiftpm/SwiftResolvedComponentDetector.cs
@@ -37,7 +37,7 @@ public class SwiftResolvedComponentDetector : FileComponentDetector, IExperiment
 
     public override IList<string> SearchPatterns { get; } = ["Package.resolved"];
 
-    public override IEnumerable<ComponentType> SupportedComponentTypes => [ComponentType.Git];
+    public override IEnumerable<ComponentType> SupportedComponentTypes => [ComponentType.Swift];
 
     public override int Version => 2;
 
@@ -79,11 +79,17 @@ public class SwiftResolvedComponentDetector : FileComponentDetector, IExperiment
                     // The version of the package is not always available.
                     var version = package.State.Version ?? package.State.Branch ?? package.State.Revision;
 
-                    var detectedGitComponent = new GitComponent(
-                        repositoryUrl: new Uri(package.Location),
-                        commitHash: package.State.Revision,
-                        tag: version);
-                    singleFileComponentRecorder.RegisterUsage(new DetectedComponent(component: detectedGitComponent));
+                    var detectedComponent = new SwiftComponent(
+                        name: package.Identity,
+                        version: version,
+                        repositoryUrl: package.Location,
+                        hash: package.State.Revision,
+                        kind: package.Kind);
+                    singleFileComponentRecorder.RegisterUsage(new DetectedComponent(component: detectedComponent));
+                }
+                else
+                {
+                    this.Logger.LogWarning("Unsupported package kind: {Kind}", package.Kind);
                 }
             }
             catch (Exception exception)
